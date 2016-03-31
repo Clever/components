aeebfc949133950bad9fb0251b832c3175a69fdc
Cosmo Wolfe <cosmo.wolfe@gmail.com>
2016-03-31 15:22:58 -0700

    Validate properties

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -2,17 +2,27 @@ import React from "react";
 
 require('./Button.less');
 
+const VALID_TYPES = ["primary", "secondary", "destructive"];
+const VALID_SIZES = ["large", "regular", "small", "regular"];
+
 class Button extends React.Component {
+  constructor(props) {
+    super()
+    if (props.type && VALID_TYPES.indexOf(props.type) === -1) {
+      throw new Error(`Invalid type property. Got ${props.type}, expected one of ${VALID_TYPES}`);
+    }
+    if (props.size && VALID_SIZES.indexOf(props.size) === -1) {
+      throw new Error(`Invalid size property. Got ${props.size}, expected one of ${VALID_SIZES}`);
+    }
+    if (props.type === "destructive" && props.size === "small"){
+      throw new Error("Small destructive buttons are not supported");
+    }
+  }
   render() {
-    var classes = "Button ";
-    if (["primary", "secondary", "destructive"].indexOf(this.props.type) > -1) {
-      classes += `Button--${this.props.type} `;
-    }
-    if (["large", "regular", "small"].indexOf(this.props.size) > -1) {
-      classes += `Button--${this.props.size} `;
-    } else {
-      classes += `Button--regular `;
-    }
+    let classes = "Button ";
+    classes += `Button--${this.props.type} `;
+    let size = ["large", "regular", "small"].indexOf(this.props.size) > -1 ? this.props.size : "regular"
+    classes += `Button--${size} `;
     if (this.props.href == null && !this.props.disabled) {
       // use <button>s for all disabled buttons and things with no href prop
       return (
