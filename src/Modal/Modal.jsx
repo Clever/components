8b40928885a964c90674be1b486a5cca2b021640
Kevin Shen <kevin.shen@clever.com>
2016-04-05 17:33:18 -0700

    Modal.jsx: add prop types

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -5,17 +5,22 @@ require("./Modal.less");
 var DEFAULT_WIDTH = 400;
 
 var Modal = React.createClass({
+  propTypes: {
+    closeModal: React.PropTypes.func.isRequired,
+    title: React.PropTypes.string,
+    width: React.PropTypes.number,
+  },
   render: function() {
     var width = this.props.width || DEFAULT_WIDTH;
+    var modalStyles = {
+      width: width + "px",
+      marginLeft: "-" + (width / 2) + "px"
+    };
+
     return (
       <div className="Modal">
         <div className="Modal--background" onClick={this.props.closeModal}/>
-        <div
-          className="Modal--window"
-          style={{
-            width: width + "px",
-            marginLeft: "-" + (width / 2) + "px"
-          }}>
+        <div className="Modal--window" style={modalStyles}>
           <div className="Modal--window--container">
             <h2>{this.props.title}</h2>
             {this.props.children}
