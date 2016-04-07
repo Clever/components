7c0f6a321323029e25931028baf153a1a7a2e49d
Omar Diab <omar.diab@clever.com>
2016-04-07 16:46:11 -0700

    make modal, docs stateless; add proptypes to modal

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -4,25 +4,30 @@ require("./Modal.less");
 
 const DEFAULT_WIDTH = 400;
 
-export class Modal {
-  render() {
-    const width = this.props.width || DEFAULT_WIDTH;
-    return (
-      <div className="Modal">
-        <div className="Modal--background" onClick={this.props.closeModal} />
-        <div
-          className="Modal--window"
-          style={{
-            width: `${width}px`,
-            marginLeft: `-${width / 2}px`,
-          }}
-        >
-          <div className="Modal--window--container">
-            <h2>{this.props.title}</h2>
-            {this.props.children}
-          </div>
+export function Modal(props) {
+  const width = props.width || DEFAULT_WIDTH;
+  return (
+    <div className="Modal">
+      <div className="Modal--background" onClick={props.closeModal} />
+      <div
+        className="Modal--window"
+        style={{
+          width: `${width}px`,
+          marginLeft: `-${width / 2}px`,
+        }}
+      >
+        <div className="Modal--window--container">
+          <h2>{props.title}</h2>
+          {props.children}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+Modal.propTypes = {
+  width: React.PropTypes.number,
+  closeModal: React.PropTypes.func.isRequired,
+  title: React.PropTypes.string.isRequired,
+  children: React.PropTypes.node.isRequired,
+};
