ce41d6f14d2188550a50f340387e76976b231ff4
Jacob Simon <jacob.anthony.simon@gmail.com>
2016-07-20 03:00:00 +0000

    Style changes

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -33,6 +33,12 @@ export class Modal extends React.Component {
   }
 
   render() {
+    let scrollable = false;
+    if (this.state.windowHeight && this.modal) {
+      if (this.modal.scrollHeight >= 0.9 * this.state.windowHeight - 70) {
+        scrollable = true;
+      }
+    }
     let windowStyle = {
       width: `${this.props.width}px`,
       marginLeft: `-${this.props.width / 2}px`,
@@ -40,16 +46,17 @@ export class Modal extends React.Component {
     // The content is max 90% of the window height less 70px (height of the header)
     let contentStyle = {
       maxHeight: (this.state.windowHeight * 0.9) - 70,
+      overflowY: scrollable ? "scroll" : "hidden",
     };
     return (
       <div className="Modal">
         <div className="Modal--background" onClick={this.props.closeModal} />
-        <div className="Modal--window" style={windowStyle} >
+        <div className="Modal--window" style={windowStyle}>
           <header className="Modal--header">
             <button className="Modal--close" onClick={this.props.closeModal} />
             <h2>{this.props.title}</h2>
           </header>
-          <div style={contentStyle} className="Modal--window--content">
+          <div style={contentStyle} className="Modal--window--content" ref={(ref) => this.modal = ref}>
             {this.props.children}
           </div>
         </div>
