9ac1ef7cef67866e06021bd77ff53b1bdc45d113
Kofi Ohene-Adu <kofi@clever.com>
2016-09-14 12:32:54 -0700

    Add git-push step to deploy script to keep master and gh-pages in sync. (#54)
    * Add git-push step to deploy script to keep master and gh-pages in sync.
    
    * Add error checking to ensure deploy.sh is run from master

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+currentBranch=$(git symbolic-ref --short HEAD)
+if [[ $currentBranch != "master" ]]
+then
+  echo -e "\n\033[0;31m✖ The deploy script must be run from the master branch.\033[0m\n";
+  exit 1;
+fi
+
+diffWithOrigin=$(git diff --name-only origin/master)
+if [[ $diffWithOrigin != "" ]]
+then
+  echo -e "\n\033[0;31m✖ Your local master branch must be in sync with origin/master.\033[0m\n";
+  exit 1;
+fi
+
 ./node_modules/.bin/webpack
 
 git add docs/
@@ -9,3 +23,7 @@ git add docs/
 git commit
 
 git subtree push --prefix docs origin gh-pages
+
+git push origin master
+
+echo -e "\n\033[0;32m✓ Deployment complete. Verify at http://clever.github.io/components\033[0m\n";
