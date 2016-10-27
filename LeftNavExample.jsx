14ef9ceae4478ba6a7acfada0051663b60075756
Jonah Kagan <jonah.kagan@clever.com>
2016-10-26 17:52:52 -0700

    Add LeftNav component

---
+++ LeftNavExample.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import {FlexBox, FlexItem, Button, LeftNav, Icon} from "../src/";
+
+import "./LeftNavExample.less";
+
+export default class LeftNavExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {selected: "Home", collapsed: false};
+  }
+
+  render() {
+    const {cssClass} = LeftNavExample;
+    const {NavLink, NavGroup} = LeftNav;
+
+    const icon = name => <Icon name={name} size={Icon.sizes.SMALL} />;
+    const link = (label, iconNode) =>
+      <NavLink
+        label={label}
+        icon={iconNode}
+        selected={this.state.selected === label}
+        onClick={() => this.setState({selected: label})}
+      />;
+
+    return (
+      <div>
+        <a name="leftnav">
+          <h1>Left Nav</h1>
+        </a>
+        <Button
+          className={cssClass.COLLAPSE}
+          type="primary"
+          value={<span className="fa fa-bars" />}
+          onClick={() => this.setState({collapsed: !this.state.collapsed})}
+        />
+        <FlexBox className={cssClass.CONTAINER}>
+          <LeftNav collapsed={this.state.collapsed}>
+            {link("Home", icon(Icon.names.SCHOOL))}
+            <NavGroup label="Tools" id="Tools" icon={icon(Icon.names.GEAR)}>
+              {link("Hammer")}
+              {link("Screwdriver")}
+              {link("Measuring Tape")}
+            </NavGroup>
+            <NavGroup label="Juggling Props" id="Juggling Props" icon={icon(Icon.names.JUGGLER)}>
+              {link("Balls")}
+              {link("Clubs")}
+              {link("Rings")}
+            </NavGroup>
+            {link("Profile", icon(Icon.names.USER))}
+          </LeftNav>
+          <FlexItem grow className={cssClass.RIGHT_PANE}>
+            <h2>{this.state.selected}</h2>
+            <p>Look at that pretty left nav!</p>
+          </FlexItem>
+        </FlexBox>
+      </div>
+    );
+  }
+}
+
+LeftNavExample.cssClass = {
+  CONTAINER: "LeftNavExample",
+  COLLAPSE: "LeftNavExample--collapse",
+  RIGHT_PANE: "LeftNavExample--rightPane",
+};
