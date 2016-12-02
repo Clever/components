f80a1e4676c9d89d8ea260a7396453aac55af7ef
CarlNelson <carl@carlrnelson.com>
2016-12-02 12:39:03 -0800

    Placeholder content for Gettting Started

--- docs/components/GettingStartedView.jsx
+++ docs/components/GettingStartedView.jsx
@@ -9,6 +9,14 @@ export default function GettingStartedView() {
   return (
     <View className={cssClass.CONTAINER} title="Getting Started">
       TODO
+      * how to install components
+      * use usage section from readme
+      CONTRIBUTING
+      Testing
+      Testing in multiple browsers (browserstack)
+      PR Template
+      Bump version
+      Manually deploying examples with deploy script
     </View>
   );
 }
