cbce4be60baa12fdc448c313482c424b35886467
CarlNelson <carl@carlrnelson.com>
2016-12-06 09:05:44 -0800

    Fixes from Kofis review

--- components/ColorsView.jsx
+++ components/ColorsView.jsx
@@ -13,15 +13,16 @@ export default class ColorsView extends PureComponent {
     return (
       <View className={cssClass.CONTAINER} title="Colors">
         <Grid>
-          <Row grow className="items--stretch margin--bottom--5xl padding--right--m">
+          <Row grow className="margin--bottom--5xl padding--right--m">
             <Col span={6} className="flexbox self--start padding--right--l">
-              <div className="display--block">
+              <div>
                 <h2>Primary Color</h2>
-                <p>This tone and shade of blue reflects the bold and inspirational nature of the Clever vision. It was also inspired by the bright colors used in many of today’s classrooms.</p>
+                <p>This tone and shade of blue reflects the bold and inspirational nature of the Clever vision. It
+                   was also inspired by the bright colors used in many of today’s classrooms.</p>
               </div>
             </Col>
             <Col span={5} className="flexbox">
-              <Row grow classname="items--stretch">
+              <Row grow>
                 <Col span={12} className="flexbox">
                   <div className="color--block large--color--block bg--primary_blue">
                     <h2 className="">Primary Blue</h2>
@@ -51,15 +52,16 @@ export default class ColorsView extends PureComponent {
               </Row>
             </Col>
           </Row>
-          <Row grow className="items--stretch margin--bottom--5xl">
+          <Row grow className="margin--bottom--5xl">
             <Col span={6} className="flexbox self--start padding--right--l">
-              <div className="display--block">
+              <div>
                 <h2>Neutral Colors</h2>
-                <p>To support the Clever Blue, these neutral colors have been chosen to add contrast and heirarchy for borders, backgrounds, and text styles.</p>
+                <p>To support the Clever Blue, these neutral colors have been chosen to add contrast and heirarchy
+                   for borders, backgrounds, and text styles.</p>
               </div>
             </Col>
             <Col span={5} className="flexbox">
-              <Row grow className="items--stretch">
+              <Row grow className="">
                 <Col span={4} className="flexbox margin--bottom--l">
                   <div className="color--block bg--neutral_black">
                     <h4 className="">@neutral_black</h4>
@@ -93,11 +95,12 @@ export default class ColorsView extends PureComponent {
               </Row>
             </Col>
           </Row>
-          <Row grow className="items--stretch margin--bottom--5xl">
+          <Row grow className="margin--bottom--5xl">
             <Col span={6} className="flexbox self--start padding--right--l">
-              <div className="display--block">
+              <div>
                 <h2>Alert Colors</h2>
-                <p>To support the Clever Blue, these neutral colors have been chosen to add contrast and heirarchy for borders, backgrounds, and text styles.</p>
+                <p>To support the Clever Blue, these neutral colors have been chosen to add contrast and heirarchy
+                   for borders, backgrounds, and text styles.</p>
               </div>
             </Col>
             <Col span={5} className="flexbox">
@@ -120,15 +123,16 @@ export default class ColorsView extends PureComponent {
               </Row>
             </Col>
           </Row>
-          <Row grow className="items--stretch margin--bottom--5xl">
+          <Row grow className="margin--bottom--5xl">
             <Col span={6} className="flexbox self--start padding--right--l">
-              <div className="display--block">
+              <div>
                 <h2>Accent Colors</h2>
-                <p>To support the Clever Blue, these neutral colors have been chosen to add contrast and heirarchy for borders, backgrounds, and text styles.</p>
+                <p>To support the Clever Blue, these neutral colors have been chosen to add contrast and heirarchy
+                   for borders, backgrounds, and text styles.</p>
               </div>
             </Col>
             <Col span={5} className="flexbox">
-              <Row grow className="items--stretch">
+              <Row grow className="">
                 <Col span={4} className="flexbox margin--bottom--l">
                   <div className="color--block bg--accent_aqua">
                     <h4 className="">@accent_aqua</h4>
@@ -147,15 +151,17 @@ export default class ColorsView extends PureComponent {
               </Row>
             </Col>
           </Row>
-          <Row grow className="items--stretch margin--bottom--5xl padding--right--m">
+          <Row grow className="margin--bottom--5xl padding--right--m">
             <Col span={6} className="flexbox self--start padding--right--l">
-              <div className="display--block">
+              <div>
                 <h2>Shades and Tints</h2>
-                <p>To provide the subtly and range necessary for interaction patterns on a modern web interface, we use a scale of shades and tints for our existing color palettes. We use a tint and shade scale which add white or black respectively to the base color in 15% increments.</p>
+                <p>To provide the subtly and range necessary for interaction patterns on a modern web interface,
+                   we use a scale of shades and tints for our existing color palettes. We use a tint and shade
+                   scale which add white or black respectively to the base color in 15% increments.</p>
               </div>
             </Col>
             <Col span={5} className="flexbox">
-              <Row grow className="items--stretch justify--left">
+              <Row grow className="justify--left">
                 <Col span={12} className="flexbox">
                   <div className="color--block large--color--block bg--alert_green">
                     <h2 className="">Example</h2>
@@ -225,13 +231,16 @@ export default class ColorsView extends PureComponent {
               </Row>
             </Col>
           </Row>
-          <Row grow className="items--stretch margin--bottom--5xl padding--right--m">
+          <Row grow className="margin--bottom--5xl padding--right--m">
             <Col span={6} className="flexbox self--start padding--right--l">
-              <div className="display--block">
+              <div>
                 <h2>Color usage</h2>
-                <p>Whenever you declare a color in LESS, you should reference the color variable instead of declaring a fixed value. If you need to use a tinted or shaded variation of a color, use the appropriate LESS mixin function.</p>
+                <p>Whenever you declare a color in LESS, you should reference the color variable instead of
+                   declaring a fixed value. If you need to use a tinted or shaded variation of a color, use
+                   the appropriate LESS mixin function.</p>
                 <strong>Tint and shade mixin functions</strong>
-                <p>You pass the mixin functions an attribute, the color variable, and a number of steps usually between 1 and 6.</p>
+                <p>You pass the mixin functions an attribute, the color variable, and a number of steps usually
+                   between 1 and 6.</p>
                 <code>.shade(background-color, @alert_red, 2);</code>
               </div>
             </Col>
