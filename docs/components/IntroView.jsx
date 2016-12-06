5cfb3665c6dff3445c42568558b735349c788ab6
CarlNelson <carl@carlrnelson.com>
2016-12-06 09:05:44 -0800

    Fixes from Kofis review

--- docs/components/IntroView.jsx
+++ docs/components/IntroView.jsx
@@ -11,34 +11,44 @@ export default function IntroView() {
   return (
     <View className={cssClass.CONTAINER} title="Say &ldquo;Hi!&rdquo; to Dewey">
       <Grid>
-        <Row grow className="items--stretch margin--bottom--xl">
+        <Row grow className="margin--bottom--xl">
           <Col span={6} className="flexbox self--start padding--right--l">
-            <div className="display--block">
+            <div>
               <h2>A design system for the ed-tech ecosytem</h2>
-              <p>As <a className="Button Button--linkPlain Button--regular" href="https://clever.com">Clever</a> grows into a mature product and company, a big challenge we have as a product organization is to create cohesive product experiences at scale that are accessible, usable, and effective for their intent.</p>
-              <p>Dewey is our source of truth for UI and product design at Clever. Everyone working at Clever is encouraged to stay familiar with this design system and help ensure that it is kept up-to-date.</p>
+              <p>As&nbsp;
+              <a 
+                className="Button Button--linkPlain Button--regular" 
+                href="https://clever.com">
+                Clever
+              </a>
+                &nbsp;grows into a mature product and company, a big challenge we have as a product organization is to
+                create cohesive product experiences at scale that are accessible, usable, and effective for their intent.</p>
+              <p>Dewey is our source of truth for UI and product design at Clever. Everyone working at Clever
+                 is encouraged to stay familiar with this design system and help ensure that it is kept up-to-date.</p>
             </div>
           </Col>
           <Col span={4} className="flexbox">
-            <Row grow className="items--stretch">
+            <Row grow>
               <Col span={12} className="flexbox margin--bottom--l">
-                <div className="display--block">
+                <div>
                   <Icon size={Icon.sizes.LARGE} name={Icon.names.TYPEWRITER} />
-                  <h3 className="text--light display--inline-block padding--left--m">Cohesive experiences</h3>
-                  <p>Create a system of coherent UI and interaction patterns to improve usability, visual consistency, and clarity instead of reinventing the radio button.</p>
+                  <h3 className="text--light display--inlineBlock padding--left--m">Cohesive experiences</h3>
+                  <p>Create a system of coherent UI and interaction patterns to improve usability, visual
+                     consistency, and clarity instead of reinventing the radio button.</p>
                 </div>
               </Col>
               <Col span={12} className="flexbox margin--bottom--l">
-                <div className="display--block">
+                <div>
                   <Icon size={Icon.sizes.LARGE} name={Icon.names.WEBSITE_HTML} />
-                  <h3 className="display--inline-block padding--left--m ">Faster development</h3>
-                  <p>Provide reusable utilities and components that accelerate the development of interfaces allowing us to focus on workflows and logic.</p>
+                  <h3 className="display--inlineBlock padding--left--m ">Faster development</h3>
+                  <p>Provide reusable utilities and components that accelerate the development of interfaces
+                     allowing us to focus on workflows and logic.</p>
                 </div>
               </Col>
               <Col span={12} className="flexbox margin--bottom--l">
-                <div className="display--block">
+                <div>
                   <Icon size={Icon.sizes.LARGE} name={Icon.names.PEN} />
-                  <h3 className="text--light display--inline-block padding--left--m">Clear documentation</h3>
+                  <h3 className="text--light display--inlineBlock padding--left--m">Clear documentation</h3>
                   <p>Showcase good examples for when to use which component or layout, and how to implement them.</p>
                 </div>
               </Col>
@@ -50,29 +60,42 @@ export default function IntroView() {
             <h2>Frequently Asked Questions</h2>
           </Col>
         </Row>
-        <Row grow className="items--stretch">
+        <Row grow>
           <Col span={6} className="flexbox padding--right--l">
-            <div className="display--block">
+            <div>
               <h3>What if I'm not using React?</h3>
-              <p>While it is highly recommended to use React for any new feature development or enhancement, it isn't always possible. In those cases, use the LESS mixins and styles to maintain consistency across our products.</p>
+              <p>While it is highly recommended to use React for any new feature development or enhancement,
+                 it isn't always possible. In those cases, use the LESS mixins and styles to maintain consistency
+                 across our products.</p>
             </div>
           </Col>
           <Col span={6} className="flexbox padding--right--l">
-            <div className="display--block">
+            <div>
               <h3>How do I contribute to Dewey?</h3>
-              <p>Great question! If you're building a brand new component, check in with the #frontend slack channel to start the conversation. If you're improving an existing component or LESS file, just use the standard PR practice and <span className="text--italic">pick a frontend for PR</span>.</p>
+              <p>Great question! If you're building a brand new component, check in with the #frontend slack
+                 channel to start the conversation. If you're improving an existing component or LESS file, just
+                 use the standard PR practice and <span className="text--italic">pick a frontend for PR</span>.</p>
             </div>
           </Col>
           <Col span={6} className="flexbox padding--right--l">
-            <div className="display--block">
+            <div>
               <h3>What if I find a bug in Dewey?</h3>
-              <p>If you can fix it quickly, go ahead and submit a quick bug fix PR. Otherwise, log a ticket in the <a className="Button Button--linkPlain Button--regular" href="https://clever.atlassian.net/secure/RapidBoard.jspa?rapidView=122">frontend board</a> referencing the component name and how to reproduce the bug.</p>
+              <p>If you can fix it quickly, go ahead and submit a quick bug fix PR. Otherwise, log a ticket in
+                 the&nbsp;
+                <a 
+                  className="Button Button--linkPlain Button--regular" 
+                  href="https://clever.atlassian.net/secure/RapidBoard.jspa?rapidView=122">
+                  frontend board
+                </a>
+                 &nbsp;referencing the component name and how to reproduce the bug.</p>
             </div>
           </Col>
           <Col span={6} className="flexbox padding--right--l">
-            <div className="display--block">
+            <div>
               <h3>Why is it called Dewey?</h3>
-              <p>Alice Chipman Dewey and John Dewey were highly influential figures in education, philosophy, and social movements in the early 20<sup>th</sup> century. We pay homage to their work and influence on the fields of design, information systems, and education.</p>
+              <p>Alice Chipman Dewey and John Dewey were highly influential figures in education, philosophy, and social
+                 movements in the early 20<sup>th</sup> century. We pay homage to their work and influence on the fields
+                 of design, information systems, and education.</p>
             </div>
           </Col>
         </Row>
--- docs/components/UITextView.jsx
+++ docs/components/UITextView.jsx
@@ -14,31 +14,33 @@ export default class UITextView extends PureComponent {
     return (
       <View className={cssClass.CONTAINER} title="UI Text">
         <Grid>
-          <Row grow className="items--stretch justify--left margin--bottom--xl">
+          <Row grow className="justify--left margin--bottom--xl">
             <Col span={6} className="flexbox padding--right--l">
-              <div className="display--block">
+              <div>
                 <h2>Interfaces are mostly text</h2>
-                <p>From buttons denoting actions to instructions for complex interfaces, the majority of an interface is communicated through text. Writing clear and actionable text takes practice.</p>
+                <p>From buttons denoting actions to instructions for complex interfaces, the majority of an
+                   interface is communicated through text. Writing clear and actionable text takes practice.</p>
               </div>
             </Col>
           </Row>
-          <Row grow className="items--stretch margin--bottom--xl justify--left">
+          <Row grow className="margin--bottom--xl justify--left">
             <Col span={12} className="flexbox self--start">
               <h2>The basics</h2>
             </Col>
             <Col span={6} className="flexbox self--start">
               <Row grow className="items--stretch">
                 <Col span={12} className="flexbox uiText--block">
-                  <div className="display--block">
+                  <div>
                     <Icon size={Icon.sizes.MEDIUM} name={Icon.names.GLASSES} />
-                    <h3 className="display--inline-block padding--left--m">Write for all users</h3>
-                    <p>Some people will read every word you write. Others will just skim. Help everyone read better by grouping related ideas together and using descriptive headers and subheaders.</p>
+                    <h3 className="display--inlineBlock padding--left--m">Write for all users</h3>
+                    <p>Some people will read every word you write. Others will just skim. Help everyone read
+                       better by grouping related ideas together and using descriptive headers and subheaders.</p>
                   </div>
                 </Col>
                 <Col span={12} className="flexbox uiText--block">
-                  <div className="display--block">
+                  <div>
                     <Icon size={Icon.sizes.MEDIUM} name={Icon.names.CHAT_HEART} />
-                    <h3 className="display--inline-block padding--left--m">Be friendly</h3>
+                    <h3 className="display--inlineBlock padding--left--m">Be friendly</h3>
                     <p>Write like a human. Don’t be afraid to break a few rules if it makes your writing more relatable.</p>
                   </div>
                 </Col>
@@ -47,23 +49,23 @@ export default class UITextView extends PureComponent {
             <Col span={4} className="flexbox justify--left">
               <Row grow className="items--stretch">
                 <Col span={8} className="flexbox uiText--block">
-                  <div className="display--block">
+                  <div>
                     <Icon size={Icon.sizes.MEDIUM} name={Icon.names.LEADER} />
-                    <h3 className="display--inline-block padding--left--m">Guide the user</h3>
+                    <h3 className="display--inlineBlock padding--left--m">Guide the user</h3>
                     <p>Think of yourself as a tour guide for our readers. Communicate in a friendly and helpful way.</p>
                   </div>
                 </Col>
                 <Col span={8} className="flexbox uiText--block">
-                  <div className="display--block">
+                  <div>
                     <Icon size={Icon.sizes.MEDIUM} name={Icon.names.PEN_PAPER} />
-                    <h3 className="display--inline-block padding--left--m">Be concise</h3>
+                    <h3 className="display--inlineBlock padding--left--m">Be concise</h3>
                     <p>Use short words and sentences. Avoid unnecessary modifiers.</p>
                   </div>
                 </Col>
                 <Col span={8} className="flexbox uiText--block">
-                  <div className="display--block">
+                  <div>
                     <Icon size={Icon.sizes.MEDIUM} name={Icon.names.BULLSEYE} />
-                    <h3 className="display--inline-block padding--left--m">Be specific</h3>
+                    <h3 className="display--inlineBlock padding--left--m">Be specific</h3>
                     <p>Avoid vague language. Cut the fluff.</p>
                   </div>
                 </Col>
