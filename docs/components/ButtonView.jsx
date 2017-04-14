f2aaf85fad529cc5f16a4a895140ab7065adae0f
CarlNelson <carl@carlrnelson.com>
2017-04-14 13:28:18 -0700

    add props to Button docs

--- docs/components/ButtonView.jsx
+++ docs/components/ButtonView.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Example from "./Example";
+import PropDocumentation from "./PropDocumentation";
 import View from "./View";
 import {Button} from "src";
 
@@ -139,6 +140,67 @@ export default function ButtonView() {
           )}
         />
       </Example>
+
+      <PropDocumentation
+        availableProps={[
+          {
+            name: "value",
+            type: "string",
+            description: "The text that appears on the button",
+          },
+          {
+            name: "type",
+            type: "String",
+            description: "One of primary, secondary, destructive, link, linkPlain, plain",
+            defaultValue: "secondary",
+          },
+          {
+            name: "size",
+            type: "String",
+            description: "One of large, regular, small",
+            defaultValue: "regular",
+          },
+          {
+            name: "onClick",
+            type: "Function",
+            description: "Called when the user clicks on the button",
+            optional: true,
+          },
+          {
+            name: "href",
+            type: "String",
+            description: "If provided, cause the button to behave as a link",
+            optional: true,
+          },
+          {
+            name: "target",
+            type:"String",
+            description: "For links, either _self or _blank",
+            defaultValue: "_blank",
+            optional: true,
+          },
+          {
+            name: "disabled",
+            type: "Bool",
+            description: "User interaction is disabled when true",
+            defaultValue: "false",
+            optional: true,
+          },
+          {
+            name: "style",
+            type: "Object",
+            description: "Add custom styles (e.g. margin) if you must",
+            optional: true,
+          },
+          {
+            name: "className",
+            type: "string",
+            description: "Optional additional classname to apply to the button",
+            optional: true,
+          },
+        ]}
+        className={cssClass.PROPS}
+      />
     </View>
   );
 }
