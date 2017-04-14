e5603e8a2337cfe0388d769e27eebd3699beed59
CarlNelson <carl@carlrnelson.com>
2017-04-14 15:18:28 -0700

    add prop docs to LeftNav

--- docs/components/LeftNavView.jsx
+++ docs/components/LeftNavView.jsx
@@ -1,6 +1,7 @@
 import React, {PureComponent} from "react";
 
 import View from "./View";
+import PropDocumentation from "./PropDocumentation";
 import LeftNavExample from "../LeftNavExample";
 
 export default class LeftNavView extends PureComponent {
@@ -10,6 +11,81 @@ export default class LeftNavView extends PureComponent {
     return (
       <View className={cssClass.CONTAINER} title="LeftNav">
         <LeftNavExample />
+
+        <PropDocumentation
+          availableProps={[
+            {
+              name: "collapsed",
+              type: "Boolean",
+              description: "Shrinks the main navigation to just icons",
+              defaultValue: false,
+              optional: true,
+            },
+            {
+              name: "children",
+              type: "Array<Union<LeftNav.NavLink,LeftNav.NavGroup",
+              description: "The links and link groups to render in the nav",
+              optional: true,
+            },
+          ]}
+          className={cssClass.PROPS}
+          title="LeftNav"
+        />
+
+        <PropDocumentation
+          availableProps={[
+            {
+              name: "icon",
+              type: "Node",
+              description: "The node to render as the link icon",
+              optional: true,
+            },
+            {
+              name: "id",
+              type: "String",
+              description: "Unique identifier for this group",
+            },
+            {
+              name: "label",
+              type: "String",
+              description: "The label to render as the link text",
+            },
+          ]}
+          className={cssClass.PROPS}
+          title="NavGroup"
+        />
+
+        <PropDocumentation
+          availableProps={[
+            {
+              name: "icon",
+              type: "Node",
+              description: "The node to render as the link icon. Top level links should have an icon.",
+              optional: true,
+            },
+            {
+              name: "label",
+              type: "String",
+              description: "The label to render as the link text",
+            },
+            {
+              name: "onClick",
+              type: "Function",
+              description: "Handler to call when the link is clicked",
+              optional: true,
+            },
+            {
+              name: "selected",
+              type: "Boolean",
+              description: "Whether or not to render the link as selected",
+              defaultValue: false,
+              optional: true,
+            }
+          ]}
+          className={cssClass.PROPS}
+          title="NavLink"
+        />
+
       </View>
     );
   }
