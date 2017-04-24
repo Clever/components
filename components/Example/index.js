35ddfab79ade841edfaa1980a8a0ae05d2de5c98
Sam Fishman <sam.fishman@clever.com>
2017-04-24 05:06:47 +0000

    Add transform-code-string.

--- components/Example/index.js
+++ components/Example/index.js
@@ -1,5 +1,6 @@
 import Example from "./Example";
+import ExampleCode from "./ExampleCode";
 import CodeSample from "./CodeSample";
 
 export default Example;
-export {CodeSample};
+export {CodeSample, ExampleCode};
