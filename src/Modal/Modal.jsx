8171e6275c68f81a29cbec47de502b7d8258fd93
Cosmo Wolfe <cosmo.wolfe@gmail.com>
2017-04-24 16:50:49 -0700

    Inline modal close icon (#165)

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -4,6 +4,7 @@ import classnames from "classnames";
 require("./Modal.less");
 
 const DEFAULT_WIDTH = 400;
+const closeIcon = <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z" /></svg>;
 
 export class Modal extends React.Component {
   constructor(props) {
@@ -46,7 +47,7 @@ export class Modal extends React.Component {
         <div className="Modal--background" onClick={this.props.closeModal} />
         <div className="Modal--window" style={windowStyle}>
           <header className="Modal--header">
-            <button className="Modal--close" onClick={this.props.closeModal} />
+            <button className="Modal--close" onClick={this.props.closeModal}>{closeIcon}</button>
             <h2>{this.props.title}</h2>
           </header>
           <div style={contentStyle} className="Modal--window--content">
