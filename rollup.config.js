793327cea1afd59cd0d280be059f5399a15ddd1d
Omar Diab <omar.diab@clever.com>
2017-05-09 20:48:12 -0700

    ignore less and css files in rollup, set external libs, remove less plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,32 @@
 import commonjs from "rollup-plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import less from "rollup-plugin-less";
 
 if (["es", "cjs"].indexOf(process.env.OUTPUT_BUILD) === -1) {
   throw new Error("Env var `OUTPUT_BUILD` must be one of ['es', 'cjs']");
 }
 
+function ignoreExtensions(list) {
+  return {
+    name: "ignoreExtensions",
+    load: id => {
+      for (const suffix of list) {
+        if (id.endsWith(suffix)) {
+          return "export default {}";
+        }
+      }
+
+      return null;
+    },
+  };
+}
 export default {
   entry: "src/index.ts",
   dest: `dist/index.${process.env.OUTPUT_BUILD}.js`,
   format: process.env.OUTPUT_BUILD,
   sourceMap: "inline",
+  external: ["react", "moment"],
   plugins: [
-    less(),
+    ignoreExtensions([".less", ".css"]),
     typescript(),
     commonjs(), // allows requiring module.exports-style code
   ],
