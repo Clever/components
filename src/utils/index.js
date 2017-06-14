17ee9ecffcf1ec12e309d59877b897ba0057bb20
Omar Diab <me@omardiab.com>
2017-06-13 22:34:28 -0400

    implement classNameFor method in utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,14 @@
+import classnames from "classnames";
+
 /**
  * Extracts properties from props that klass's propTypes field specifies, as an object.
  * Useful for inheriting properties from other components; see ModalButton for examples.
  */
 export function propsFor(klass, props) {
   return Object.keys(props).reduce((prev, propKey) => {
-    if (Object.keys(klass.propTypes).indexOf(propKey) >= 0) prev[propKey] = props[propKey];
+    if (Object.keys(klass.propTypes).indexOf(propKey) >= 0) {
+      prev[propKey] = props[propKey];
+    }
     return prev;
   }, {});
 }
@@ -21,14 +25,15 @@ export function omitKeys(obj, ...toOmit) {
   }, {});
 }
 
-
 /**
  * Returns a copy of obj (Object) with prefix (String) prepended to each key. Capitalizes the first
  * letter of the old key if capitalizeFirst (bool) is true.
  */
 export function prefixKeys(obj, prefix, capitalizeFirst = true) {
   return Object.keys(obj).reduce((prev, key) => {
-    const suffix = capitalizeFirst ? key.charAt(0).toUpperCase() + key.slice(1) : key;
+    const suffix = capitalizeFirst
+      ? key.charAt(0).toUpperCase() + key.slice(1)
+      : key;
     prev[prefix + suffix] = obj[key];
     return prev;
   }, {});
@@ -43,10 +48,40 @@ export function unprefixKeys(obj, prefixToRemove, lowercaseFirst = true) {
   return Object.keys(obj).reduce((prev, key) => {
     if (key.indexOf(prefixToRemove) === 0) {
       const sansPrefix = key.slice(prefixToRemove.length);
-      const newKey = lowercaseFirst ? sansPrefix.charAt(0).toLowerCase() +
-        sansPrefix.slice(1) : sansPrefix;
+      const newKey = lowercaseFirst
+        ? sansPrefix.charAt(0).toLowerCase() + sansPrefix.slice(1)
+        : sansPrefix;
       prev[newKey] = obj[key];
     }
     return prev;
   }, {});
+}
+
+/**
+ * Combine classname strings with long kabobs (--), and prepend each
+ * prefix to the result.
+ * For example:
+ * ```
+ * // returns "Component--a--b MyCustomClassName--a--b"
+ * classNamesFor(["Component", "MyCustomClassName"], "a", "b")
+ * ```
+ * If no prefixes are provided (i.e. prefixes === null or prefixes only contains null/empty values)
+ * it just doesn't append a prefix—i.e. it kabobs the rest of the arguments together.
+ * @param {string | Array<string>} prefixes Prefixes to prepend to resulting classname
+ * @param {string | Array<string>} classSegments Class names to kabob together
+ */
+export function classNameFor(prefixes, classSegments) {
+  const prefixList = (prefixes instanceof Array ? prefixes : [prefixes]).filter(p => !!p);
+  let classSegmentList;
+  if (!classSegments) {
+    classSegmentList = [];
+  } else {
+    classSegmentList = (classSegments instanceof Array ? classSegments : [classSegments]);
+  }
+
+  if (prefixList.length === 0) {
+    return classSegmentList.join("--");
+  }
+
+  return classnames(prefixList.map(p => [p].concat(classSegmentList).join("--")));
 }
