2de0619098796fe01b338e1ccc2a3e257cb5d915
prime-time <32328921+prime-time@users.noreply.github.com>
2017-10-17 18:03:30 -0700

    Modal a11y (#235)
    added focus-modal-react as toggleable feature

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import classnames from "classnames";
+import FocusTrap from "focus-trap-react";
 
 require("./Modal.less");
 
 const DEFAULT_WIDTH = 400;
 const closeIcon = <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z" /></svg>;
+const ESC = 27;
 
 export class Modal extends React.Component {
   constructor(props) {
@@ -26,7 +28,7 @@ export class Modal extends React.Component {
   }
 
   handleKeyUp(e) {
-    if (e.keyCode === 27) {
+    if (e.keyCode === ESC) {
       this.props.closeModal();
     }
   }
@@ -42,13 +44,13 @@ export class Modal extends React.Component {
     };
     // The content is max 90% of the window height less 60px (height of the header)
     let contentStyle = {maxHeight: (this.state.windowHeight * 0.9) - 60};
-    return (
+    const modalContent = (
       <div className={classnames("Modal", this.props.className)}>
         <div className="Modal--background" onClick={this.props.closeModal} />
         <div className="Modal--window" style={windowStyle}>
           <header className="Modal--header">
             <button className="Modal--close" onClick={this.props.closeModal}>{closeIcon}</button>
-            <h2>{this.props.title}</h2>
+            <h1>{this.props.title}</h1>
           </header>
           <div style={contentStyle} className="Modal--window--content">
             {this.props.children}
@@ -56,6 +58,13 @@ export class Modal extends React.Component {
         </div>
       </div>
     );
+    let modal;
+    if (this.props.focusLocked) {
+      modal = <FocusTrap>{modalContent}</FocusTrap>;
+    } else {
+      modal = modalContent;
+    }
+    return modal;
   }
 }
 
@@ -65,8 +74,10 @@ Modal.propTypes = {
   title: React.PropTypes.string.isRequired,
   closeModal: React.PropTypes.func.isRequired,
   children: React.PropTypes.node.isRequired,
+  focusLocked: React.PropTypes.bool,
 };
 
 Modal.defaultProps = {
   width: DEFAULT_WIDTH,
+  focusLocked: false,
 };
