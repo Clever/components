9362055b78704ff5ebf1896e0f5e032b3ef22b6e
Kofi Ohene-Adu <kofi@clever.com>
2018-01-02 10:34:20 +0000

    Goals logo (#246)
    * Generate new component skeleton.
    
    * Add component implementation/styling
    
    * Bump patch version

---
+++ components/GoalsLogoView.jsx
@@ -0,0 +1,107 @@
+import * as React from "react";
+
+import Example, {CodeSample, ExampleCode} from "./Example";
+import PropDocumentation from "./PropDocumentation";
+import View from "./View";
+import {GoalsLogo, FlexBox, ItemAlign, SegmentedControl} from "src";
+
+import "./GoalsLogoView.less";
+
+const cssClass = {
+  CONFIG_CONTAINER: "GoalsLogoView--configContainer",
+  CONFIG_OPTIONS: "GoalsLogoView--configOptions",
+  CONFIG: "GoalsLogoView--config",
+  CONFIG_TOGGLE: "GoalsLogoView--configToggle",
+  CONTAINER: "GoalsLogoView",
+  INTRO: "GoalsLogoView--intro",
+  LOGO_CONTAINER: "GoalsLogoView--logoContainer",
+  PROPS: "GoalsLogoView--props",
+};
+
+export default class GoalsLogoView extends React.PureComponent {
+  static cssClass = cssClass;
+
+  state = {
+    size: GoalsLogo.defaultProps.size,
+  };
+
+  render() {
+    const {size} = this.state;
+
+    return (
+      <View
+        className={cssClass.CONTAINER}
+        title="GoalsLogo"
+        sourcePath="src/GoalsLogo/GoalsLogo.tsx"
+      >
+        <header className={cssClass.INTRO}>
+          <p>Brand logo for the Clever Goals product.</p>
+          <CodeSample>
+            {`
+              import {GoalsLogo} from "clever-components";
+              // OR
+              import GoalsLogo from "clever-components/dist/GoalsLogo"; // Avoids importing all of clever-components.
+            `}
+          </CodeSample>
+        </header>
+
+        <Example title="Basic Usage:">
+          <div className={cssClass.LOGO_CONTAINER}>
+            <ExampleCode>
+              <GoalsLogo className="my--custom--class" size={size} />
+            </ExampleCode>
+          </div>
+          {this._renderConfig()}
+        </Example>
+
+        {this._renderProps()}
+      </View>
+    );
+  }
+
+  _renderConfig() {
+    const {size} = this.state;
+
+    return (
+      <FlexBox alignItems={ItemAlign.CENTER} className={cssClass.CONFIG_CONTAINER} wrap>
+        <div className={cssClass.CONFIG}>
+          Size:
+          <SegmentedControl
+            className={cssClass.CONFIG_OPTIONS}
+            onSelect={value => this.setState({size: value})}
+            options={[
+              {content: "Small", value: "small"},
+              {content: "Medium", value: "medium"},
+              {content: "Large", value: "large"},
+            ]}
+            value={size}
+          />
+        </div>
+      </FlexBox>
+    );
+  }
+
+  _renderProps() {
+    return (
+      <PropDocumentation
+        title="<GoalsLogo /> Props"
+        availableProps={[
+          {
+            name: "className",
+            type: "string",
+            description: "Optional additional CSS class name to apply to the container.",
+            optional: true,
+          },
+          {
+            name: "size",
+            type: "GoalsLogo.Size",
+            description: "Logo size",
+            defaultValue: "GoalsLogo.Size.M",
+            optional: true,
+          },
+        ]}
+        className={cssClass.PROPS}
+      />
+    );
+  }
+}
