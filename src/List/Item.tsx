5f15ddc85596310419fc518fe5a3983542e03903
johnhuangclever <john.huang@clever.com>
2018-02-13 10:15:11 -0800

    add onClick to list item (#266)
    * add onClick to list item
    
    * more accessible onclick changes
    
    * use tint
    
    * optional class name for items container
    
    * PR comments
    
    * cleanup
    
    * use !! for boolean
    
    * lint
    
    * patch version bump

--- src/List/Item.tsx
+++ src/List/Item.tsx
@@ -8,21 +8,34 @@ export default class Item extends React.PureComponent {
   static propTypes = {
     children: PropTypes.node.isRequired,
     className: PropTypes.string,
+    onClick: PropTypes.func,
   };
 
   static cssClass = {
     CONTAINER: "List--Item",
+    CONTENT_WRAPPER: "List--Item--ContentWrapper",
+    CONTENT_ONCLICK: "List--Item--ContentWrapperClickable",
 
     type: type => `List--Item--${type}`,
   };
 
   render() {
     const {cssClass} = Item;
-    const {children, className} = this.props;
+    const {children, className, onClick} = this.props;
+
+    let Wrapper = "div";
+    if (onClick) {
+      Wrapper = "button";
+    }
 
     return (
       <li className={classnames(cssClass.CONTAINER, className)}>
-        {children}
+        <Wrapper
+          className={classnames(cssClass.CONTENT_WRAPPER, !!onClick && cssClass.CONTENT_ONCLICK)}
+          onClick={onClick}
+        >
+          {children}
+        </Wrapper>
       </li>
     );
   }
