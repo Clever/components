6af9272faf9d9e2d991beffdafa0907e0ad60b13
Kofi Ohene-Adu <kofi.ohene-adu@clever.com>
2018-03-22 12:08:38 -0700

    Add auto-generator for color variables and output both LESS and JS.

---
+++ src/utils/Colors.ts
@@ -0,0 +1,62 @@
+/**
+ * ====  NOTE: Auto-generated file. Do NOT edit directly! ====
+ * ====  Edit genColors.ts instead and run make generate. ====
+ *
+ * Standard color variables.
+ */
+
+const Colors = {
+  // Primary colors:
+  PRIMARY_BLUE: "#2e00d9",
+  PRIMARY_BLUE_SHADE_1: "#0500b0",
+  PRIMARY_BLUE_SHADE_2: "#000087",
+  PRIMARY_BLUE_TINT_1: "#3e14fa",
+  PRIMARY_BLUE_TINT_2: "#473dff",
+
+  // Neutral colors:
+  NEUTRAL_BLACK: "#15131c",
+  NEUTRAL_DARK_GRAY: "#474c5e",
+  NEUTRAL_GRAY: "#71738a",
+  NEUTRAL_SILVER: "#d6d7de",
+  NEUTRAL_OFF_WHITE: "#fbfafc",
+  NEUTRAL_WHITE: "#ffffff",
+
+  // Alert colors:
+  ALERT_GREEN: "#009e78",
+  ALERT_GREEN_SHADE_1: "#00754f",
+  ALERT_GREEN_SHADE_2: "#004c26",
+  ALERT_GREEN_TINT_1: "#14b28c",
+  ALERT_GREEN_TINT_2: "#3ddbb5",
+  ALERT_ORANGE: "#ed8a00",
+  ALERT_ORANGE_SHADE_1: "#d97600",
+  ALERT_ORANGE_SHADE_2: "#b04d00",
+  ALERT_ORANGE_TINT_1: "#ff9e14",
+  ALERT_ORANGE_TINT_2: "#ffc73d",
+  ALERT_RED: "#de004b",
+  ALERT_RED_SHADE_1: "#ca0037",
+  ALERT_RED_SHADE_2: "#a1000e",
+  ALERT_RED_TINT_1: "#f51152",
+  ALERT_RED_TINT_2: "#ff4268",
+
+  // Accent colors:
+  ACCENT_TEAL: "#00c7c7",
+  ACCENT_TEAL_SHADE_1: "#00b3b3",
+  ACCENT_TEAL_SHADE_2: "#008a8a",
+  ACCENT_TEAL_TINT_1: "#14dbdb",
+  ACCENT_TEAL_TINT_2: "#29f0f0",
+  ACCENT_PINK: "#f70099",
+  ACCENT_PINK_SHADE_1: "#e30085",
+  ACCENT_PINK_SHADE_2: "#ba005c",
+  ACCENT_PINK_TINT_1: "#ff29ae",
+  ACCENT_PINK_TINT_2: "#ff52ce",
+  ACCENT_PURPLE: "#8000ff",
+  ACCENT_PURPLE_SHADE_1: "#6c00eb",
+  ACCENT_PURPLE_SHADE_2: "#4e00c2",
+  ACCENT_PURPLE_TINT_1: "#9414ff",
+  ACCENT_PURPLE_TINT_2: "#bd3dff",
+
+  // DEPRECATED COLORS:
+  ACCENT_AQUA: "#00c7c7",
+};
+
+export default Colors;
