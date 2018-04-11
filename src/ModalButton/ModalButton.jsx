0a1c93e7ade53c8123c733fa0464ad84eafe5020
Kofi Ohene-Adu <kofi.ohene-adu@clever.com>
2018-04-11 14:06:26 -0700

    Optimise imports
    Make sure components only ever reference other components directly instead of importing from src/index.js.
    
    Avoids pulling in the entire codebase when just one comopnent is imported.
    
    Similarly, update any LESS imports that should be importing by `reference` instead of importing the entire source for other LESS files.

--- src/ModalButton/ModalButton.jsx
+++ src/ModalButton/ModalButton.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import * as PropTypes from "prop-types";
 import classnames from "classnames";
 
-import {Button, Modal} from "..";
+import {Button} from "../Button/Button";
+import {Modal} from "../Modal/Modal";
 import {omitKeys, prefixKeys, propsFor, unprefixKeys} from "../utils";
 
 const excludeModalProps = ["closeModal", "children"];
