ff2073ab6e052e9212034ac8f320e7e3e5e8d16d
mtgao2 <michael.gao@clever.com>
2018-11-09 13:00:36 -0800

    PR comments

--- src/EditableInfoPanel/EditableInfoPanel.tsx
+++ src/EditableInfoPanel/EditableInfoPanel.tsx
@@ -10,7 +10,8 @@ import "./EditableInfoPanel.less";
 const propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
-  onPerformAction: PropTypes.func.isRequired,
+  title: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
 };
 
 const cssClass = {
@@ -19,18 +20,9 @@ const cssClass = {
   CONTENT: "EditableInfoPanel--content",
 };
 
-/**
- * TODO: Add short description.
- */
 export default class EditableInfoPanel extends React.PureComponent {
   static propTypes = propTypes;
   static cssClass = cssClass;
-
-  // TODO: Uncomment if this component is stateful. Remove otherwise.
-  // state = {
-  //   sampleState1: false,
-  //   sampleState2: "foo",
-  // };
 
   render() {
     const {title, children, className, onClick} = this.props;
