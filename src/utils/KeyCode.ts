e5e6b11183599760bc3dc6c757676299f0d42aac
Kofi Ohene-Adu <kofi.ohene-adu@clever.com>
2018-12-06 12:25:38 -0800

    Add support for IE-specific KeyCodes as well...sigh

--- src/utils/KeyCode.ts
+++ src/utils/KeyCode.ts
@@ -3,9 +3,13 @@
  * NOTE: Please keep sorted for easy scanning.
  */
 const KeyCode = {
+  ARROW_DOWN_IE: "Down",
   ARROW_DOWN: "ArrowDown",
+  ARROW_LEFT_IE: "Left",
   ARROW_LEFT: "ArrowLeft",
+  ARROW_RIGHT_IE: "Right",
   ARROW_RIGHT: "ArrowRight",
+  ARROW_UP_IE: "Up",
   ARROW_UP: "ArrowUp",
   J: "j",
   K: "k",
