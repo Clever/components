ac18cf80cefc05b0c33542934ed268d84a5f7566
Vynnie Kong <vynnie.kong@clever.com>
2018-12-21 17:10:30 -0800

    Stepper component

---
+++ src/Stepper/Stepper.tsx
@@ -0,0 +1,88 @@
+import * as classnames from "classnames";
+import * as React from "react";
+import * as PropTypes from "prop-types";
+import {Sticky, StickyContainer} from "react-sticky";
+
+import Step from "./Step";
+
+import "./Stepper.less";
+
+const propTypes = {
+  className: PropTypes.string,
+  style: PropTypes.object,
+  steps: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    showTitle: PropTypes.bool,
+    showMessage: PropTypes.bool,
+    showDescription: PropTypes.bool,
+    success: PropTypes.bool,
+    warning: PropTypes.bool,
+    optional: PropTypes.bool,
+  })).isRequired,
+  currentStep: PropTypes.number,
+  seekable: PropTypes.bool,
+  stickySidebar: PropTypes.bool,
+  onStepClick: PropTypes.func,
+};
+
+const defaultProps = {
+  currentStep: 0,
+};
+
+const cssClass = {
+  CONTAINER: "Stepper",
+  SIDEBAR: "Stepper--sidebar",
+  STEPS_DISPLAY: "Stepper--stepsDisplay",
+};
+
+export default class Stepper extends React.PureComponent {
+  static propTypes = propTypes;
+  static defaultProps = defaultProps;
+  static cssClass = cssClass;
+
+  _renderSidebar(style) {
+    const {currentStep, steps, seekable, onStepClick, className} = this.props;
+
+    return (
+      <div style={style}>
+        <ul className={cssClass.STEPS_DISPLAY}>
+          {steps.map((step, idx) =>
+            <li key={idx}>
+              <Step
+                className={className}
+                title={step.title}
+                description={step.description}
+                showTitle={step.showTitle}
+                showMessage={step.showMessage}
+                showDescription={step.showDescription}
+                index={idx}
+                success={step.success}
+                current={idx === currentStep}
+                warning={step.warning}
+                optional={step.optional}
+                seekable={seekable}
+                onClick={onStepClick}
+              />
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+
+  render() {
+    const {className, style, stickySidebar} = this.props;
+
+    return (
+      <StickyContainer className={classnames(cssClass.CONTAINER, className)} style={style}>
+        <div className={cssClass.SIDEBAR}>
+          {!stickySidebar ? this._renderSidebar(style) :
+            <Sticky topOffset={24}>
+              {({style: sidebarStyle}) => this._renderSidebar(sidebarStyle)}
+            </Sticky>}
+        </div>
+      </StickyContainer>
+    );
+  }
+}
