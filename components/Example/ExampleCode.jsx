e05ad19637ae5e45ccd7f1a6d3d8f8ce77603f87
Luther Banner III <luther.banner@clever.com>
2019-01-30 15:00:58 -0800

    Update example code component to take in custom style

--- components/Example/ExampleCode.jsx
+++ components/Example/ExampleCode.jsx
@@ -6,11 +6,12 @@ import * as PropTypes from "prop-types";
  * ExampleCode allows you to specify what portion of code to reveal in the code
  * window.
  */
-export default function ExampleCode({children}) {
-  return <div>{children}</div>;
+export default function ExampleCode({children, style}) {
+  return <div style={style}>{children}</div>;
 }
 
 ExampleCode.propTypes = {
+  style: PropTypes.object,
   children: PropTypes.oneOfType([
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node),
