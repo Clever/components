cde032e3aee939f2eab91ddc0c16eb8d87266692
Luther Banner III <luther.banner@clever.com>
2019-01-30 15:03:58 -0800

    Create content for example WizardLayout

---
+++ components/WizardLayoutContent.jsx
@@ -0,0 +1,295 @@
+import * as React from "react";
+
+import {FlexBox, ItemAlign, SegmentedControl, DatePicker, TextInput, Button, Icon} from "src";
+
+// SET ACCESS CONTENT
+const appAccess = (
+  <FlexBox column>
+    <FlexBox alignItems={ItemAlign.CENTER} style={{marginBottom: "2rem"}}>
+      <FlexBox style={{height: 60, width: 60, backgroundColor: "#5582F7", marginRight: "1rem"}} />
+      <FlexBox column style={{marginRight: "3rem"}}>
+        <span>
+          <strong>Dreambox</strong> will have access to:
+            </span>
+        <p>all profile and roster data</p>
+      </FlexBox>
+      <FlexBox alignItems={ItemAlign.BASELINE}>
+        <span>Set access by: </span>
+        <SegmentedControl
+          options={[
+            {
+              content: "Recommended settings",
+              value: 0,
+            },
+            {
+              content: "Sections",
+              value: 1,
+            },
+            {
+              content: "District",
+              value: 2,
+            },
+          ]}
+        />
+      </FlexBox>
+    </FlexBox>
+    <FlexBox alignItems={ItemAlign.CENTER} style={{marginBottom: "2rem"}}>
+      <FlexBox style={{height: 60, width: 60, backgroundColor: "#5582F7", marginRight: "1rem"}} />
+      <FlexBox column style={{marginRight: "3rem"}}>
+        <span>
+          <strong>Dreambox</strong> will have access to:
+            </span>
+        <p>all profile and roster data</p>
+      </FlexBox>
+      <FlexBox alignItems={ItemAlign.BASELINE}>
+        <span>Set access by: </span>
+        <SegmentedControl
+          options={[
+            {
+              content: "Recommended settings",
+              value: 0,
+            },
+            {
+              content: "Sections",
+              value: 1,
+            },
+            {
+              content: "District",
+              value: 2,
+            },
+          ]}
+        />
+      </FlexBox>
+    </FlexBox>
+    <FlexBox alignItems={ItemAlign.CENTER} style={{marginBottom: "2rem"}}>
+      <FlexBox style={{height: 60, width: 60, backgroundColor: "#5582F7", marginRight: "1rem"}} />
+      <FlexBox column style={{marginRight: "3rem"}}>
+        <span>
+          <strong>Dreambox</strong> will have access to:
+            </span>
+        <p>all profile and roster data</p>
+      </FlexBox>
+      <FlexBox alignItems={ItemAlign.BASELINE}>
+        <span>Set access by: </span>
+        <SegmentedControl
+          options={[
+            {
+              content: "Recommended settings",
+              value: 0,
+            },
+            {
+              content: "Sections",
+              value: 1,
+            },
+            {
+              content: "District",
+              value: 2,
+            },
+          ]}
+        />
+      </FlexBox>
+    </FlexBox>
+    <FlexBox alignItems={ItemAlign.CENTER} style={{marginBottom: "2rem"}}>
+      <FlexBox style={{height: 60, width: 60, backgroundColor: "#5582F7", marginRight: "1rem"}} />
+      <FlexBox column style={{marginRight: "3rem"}}>
+        <span>
+          <strong>Dreambox</strong> will have access to:
+            </span>
+        <p>all profile and roster data</p>
+      </FlexBox>
+      <FlexBox alignItems={ItemAlign.BASELINE}>
+        <span>Set access by: </span>
+        <SegmentedControl
+          options={[
+            {
+              content: "Recommended settings",
+              value: 0,
+            },
+            {
+              content: "Sections",
+              value: 1,
+            },
+            {
+              content: "District",
+              value: 2,
+            },
+          ]}
+        />
+      </FlexBox>
+    </FlexBox>
+    <FlexBox alignItems={ItemAlign.CENTER} style={{marginBottom: "2rem"}}>
+      <FlexBox style={{height: 60, width: 60, backgroundColor: "#5582F7", marginRight: "1rem"}} />
+      <FlexBox column style={{marginRight: "3rem"}}>
+        <span>
+          <strong>Dreambox</strong> will have access to:
+            </span>
+        <p>all profile and roster data</p>
+      </FlexBox>
+      <FlexBox alignItems={ItemAlign.BASELINE}>
+        <span>Set access by: </span>
+        <SegmentedControl
+          options={[
+            {
+              content: "Recommended settings",
+              value: 0,
+            },
+            {
+              content: "Sections",
+              value: 1,
+            },
+            {
+              content: "District",
+              value: 2,
+            },
+          ]}
+        />
+      </FlexBox>
+    </FlexBox>
+  </FlexBox>
+);
+
+const portalURL = (
+  <FlexBox>
+    <FlexBox style={{marginRight: "1rem"}}>
+      <img src="./assets/img/portalScreenshot.png" />
+    </FlexBox>
+    <FlexBox column>
+      <p>
+        Update the link with a short URL, like your district's name, so students and teachers can easily access
+         all their leaning applications from their portal.
+          </p>
+      <br />
+      <FlexBox alignItems={ItemAlign.BASELINE} style={{color: "#474C5E"}}>
+        <span style={{marginRight: -12}}>https://clever.com/in/</span>
+        <TextInput style={{color: "#474C5E", border: "0.0625rem solid #e3e6eb"}} readOnly name="shortname" value="somedistrict" />
+      </FlexBox>
+    </FlexBox>
+  </FlexBox>
+);
+
+const setAccessHelp = (
+  <FlexBox column>
+    If you can't figure out what settings to use, remember applications provide recommended settings to try out!
+      </FlexBox>
+);
+
+// Clever SSO Content
+const cleverSSO = (
+  <FlexBox column>
+    <img src="./assets/img/portalExample.gif" />
+    <br />
+    <p>
+      Clever single sign-on (SSO) saves thousands of hours of classroom time and gives your
+      districts' students and teachers secure one-click access into all of their online
+      learning programs.
+        </p>
+    <div>
+      <FlexBox>
+        <Icon size={Icon.sizes.SMALL} name={Icon.names.CHECKMARK} /> Use existing accounts from Google,
+          Microsoft 365, and other IDPs to log in
+          </FlexBox>
+      <FlexBox>
+        <Icon size={Icon.sizes.SMALL} name={Icon.names.CHECKMARK} /> Give students access to a secure, personalized Portal
+          </FlexBox>
+      <FlexBox>
+        <Icon size={Icon.sizes.SMALL} name={Icon.names.CHECKMARK} /> Get direct logins into Chromebooks and Windows devices
+          </FlexBox>
+    </div>
+    <br />
+    <p>
+      Best yet, it only takes less than 10 minutes to set up and is available at no cost!
+        </p>
+    <br />
+    <Button
+      type={Button.Type.PRIMARY}
+      value="Request more info"
+    />
+  </FlexBox>
+);
+
+const singleSignOnHelp = (
+  <FlexBox column>
+    If you're interested in learning more, click the button to fill out our form! Otherwise, you can just hit "Next step."
+      </FlexBox>
+);
+
+// CLEVER GOALS CONTENT
+const cleverGoals = (
+  <FlexBox column>
+    <img src="./assets/img/goals.gif" />
+    <p>
+      Goals turns learning software into student achievement
+        </p>
+    <div>
+      <FlexBox>
+        <Icon size={Icon.sizes.SMALL} name={Icon.names.CHECKMARK} /> Set personalized student goals
+          </FlexBox>
+      <FlexBox>
+        <Icon size={Icon.sizes.SMALL} name={Icon.names.CHECKMARK} /> Increase software engagement
+          </FlexBox>
+      <FlexBox>
+        <Icon size={Icon.sizes.SMALL} name={Icon.names.CHECKMARK} />Improve learning outcomes
+          </FlexBox>
+    </div>
+    <br />
+    <Button
+      type={Button.Type.SECONDARY}
+      value={"Request demo"}
+    />
+  </FlexBox>
+);
+
+const goalsHelp = (
+  <FlexBox column>
+    If you're interested in receiveing a Goals demo, click the button to fill out our form! Otherwise, you can just hit "Next step."
+      </FlexBox>
+);
+
+
+export const WizardLayoutContent = {
+  firstStep: "setAccess",
+  setAccess: {
+    nextStep: "singleSignOn",
+    contentElems: [
+      {
+        title: "Set application launch date",
+        subtitle: "Once launched, your students and teachers will be able to see their new applications on the portal.",
+        children: <FlexBox style={{maxWidth: "15rem"}} column><p>Launch date</p><DatePicker /></FlexBox>,
+      },
+      {
+        title: "Set application access",
+        subtitle: "Define who should have access to the applications below.",
+        children: appAccess,
+      },
+      {
+        title: "Set your Clever Portal URL",
+        subtitle: "Students and teachers will log in at this URL.",
+        children: portalURL,
+      },
+    ],
+    helpContent: setAccessHelp,
+  },
+  singleSignOn: {
+    nextStep: "goals",
+    prevStep: "setAccess",
+    contentElems: [
+      {
+        title: "Clever SSO",
+        subtitle: "Find out how to start saving hours of classroom time.",
+        children: cleverSSO,
+      },
+    ],
+    helpContent: singleSignOnHelp,
+  },
+  goals: {
+    prevStep: "singleSignOn",
+    contentElems: [
+      {
+        title: "Clever Goals",
+        subtitle: "Is your district leadership asking whether these programs are being used effectively?",
+        children: cleverGoals,
+      },
+    ],
+    helpContent: goalsHelp,
+  },
+};
