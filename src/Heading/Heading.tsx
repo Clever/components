28ba250ae37dbf5997072dfe3541442fdb368abf
Mallika Mathur <mkmathur@users.noreply.github.com>
2019-01-29 18:48:53 -0800

    remove fluff

--- src/Heading/Heading.tsx
+++ src/Heading/Heading.tsx
@@ -1,20 +1,11 @@
-import * as classnames from "classnames";
 import * as PropTypes from "prop-types";
 import * as React from "react";
-
-import {Button} from "../Button/Button";
 
 import "./Heading.less";
 
 const propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
-  onPerformAction: PropTypes.func.isRequired,
-};
-
-const cssClass = {
-  CONTAINER: "Heading",
-  SUB_ELEMENT: "Heading--subElement",
 };
 
 /**
@@ -22,31 +13,14 @@ const cssClass = {
  */
 export default class Heading extends React.PureComponent {
   static propTypes = propTypes;
-  static cssClass = cssClass;
-
-  // TODO: Uncomment if this component is stateful. Remove otherwise.
-  // state = {
-  //   sampleState1: false,
-  //   sampleState2: "foo",
-  // };
 
   render() {
     const {children, className} = this.props;
 
     return (
-      <div className={classnames(cssClass.CONTAINER, className)}>
-        <h3>TODO</h3>
-        <p>{children}</p>
-        <div className={cssClass.SUB_ELEMENT}>
-          <Button
-            onClick={this._handlePerformAction}
-            type={Button.Type.PRIMARY}
-            value="Do something"
-          />
-        </div>
-      </div>
+      <h1 className={className}>
+        {children}
+      </h1>
     );
   }
-
-  _handlePerformAction = () => this.props.onPerformAction("action performed");
 }
