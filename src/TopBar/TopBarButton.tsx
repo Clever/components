031822e606f15f4d9e07efc890c8e70657e3d702
Cosmo Wolfe <cosmo.wolfe@gmail.com>
2019-05-02 12:30:46 -0700

    New header (#371)

--- src/TopBar/TopBarButton.tsx
+++ src/TopBar/TopBarButton.tsx
@@ -3,60 +3,20 @@ import * as classnames from "classnames";
 import * as PropTypes from "prop-types";
 import * as React from "react";
 
-import Colors from "../utils/Colors";
-
 import "./TopBarButton.less";
-
-const MarkerShape = {
-  CIRCLE: "circle",
-  TRIANGLE: "triangle",
-};
 
 const propTypes = {
   active: PropTypes.bool,
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
   className: PropTypes.string,
   component: PropTypes.any,
   href: PropTypes.string,
-  markerShape: PropTypes.oneOf(Object.values(MarkerShape)),
   onClick: PropTypes.func,
-  target: PropTypes.string,
+  round: PropTypes.bool,
 };
 
-const defaultProps = {
-  markerShape: MarkerShape.CIRCLE,
-};
-
-const cssClass = {
-  ACTIVE_INDICATOR: "TopBarButton--activeIndicator",
-  ACTIVE_INDICATOR_SHAPE: "TopBarButton--activeIndicator--shape",
-  MARKER: "TopBarButton--marker",
-  MARKER_SHAPE: "TopBarButton--marker--shape",
-  MARKER_CIRCLE: "TopBarButton--marker--circle",
-  MARKER_TRIANGLE: "TopBarButton--marker--triangle",
-  CONTAINER: "TopBarButton",
-};
-
-const Markers = {
-  [MarkerShape.CIRCLE]: (
-    <svg className={classnames(cssClass.MARKER, cssClass.MARKER_CIRCLE)} viewBox="0 0 100 100">
-      <circle cx={50} cy={50} r={50} fill={Colors.NEUTRAL_WHITE} />
-      <circle className={cssClass.MARKER_SHAPE} cx={50} cy={50} r={35} />
-    </svg>
-  ),
-  [MarkerShape.TRIANGLE]: (
-    <svg className={classnames(cssClass.MARKER, cssClass.MARKER_TRIANGLE)} viewBox="0 0 100 50">
-      <polygon className={cssClass.MARKER_SHAPE} points="50,0 100,50 0,50" />
-    </svg>
-  ),
-};
-
-export default class TopBarButton extends React.PureComponent {
+export class TopBarButton extends React.PureComponent {
   static propTypes = propTypes;
-  static defaultProps = defaultProps;
-  static cssClass = cssClass;
-
-  static MarkerShape = MarkerShape;
 
   _containerRef;
 
@@ -67,7 +27,7 @@ export default class TopBarButton extends React.PureComponent {
   }
 
   render() {
-    const { active, children, className, href, markerShape, onClick, target } = this.props;
+    const { active, children, className, href, onClick } = this.props;
     const additionalProps = _.omit(this.props, Object.keys(propTypes));
 
     const Wrapper = this._getWrapperComponent();
@@ -75,21 +35,22 @@ export default class TopBarButton extends React.PureComponent {
     return (
       <Wrapper
         {...additionalProps}
-        className={classnames(cssClass.CONTAINER, className)}
+        className={classnames(
+          "TopBarButton",
+          className,
+          this.props.round ? "TopBarButton--rounded" : null,
+        )}
         href={href}
         onClick={onClick}
-        ref={this._handleContainerRef}
-        target={target}
+        ref={ref => {
+          this._containerRef = ref;
+        }}
       >
         {children}
-        {active && Markers[markerShape]}
+        {active && <div className="TopBarButton--activeIndicator" />}
       </Wrapper>
     );
   }
-
-  _handleContainerRef = ref => {
-    this._containerRef = ref;
-  };
 
   _getWrapperComponent() {
     const { component, href } = this.props;
---
+++ src/TopBar/index.tsx
@@ -0,0 +1,45 @@
+import * as classnames from "classnames";
+import * as PropTypes from "prop-types";
+import * as React from "react";
+
+import Logo from "../Logo";
+import { FlexBox, Justify } from "../flex";
+import { TopBarButton } from "./TopBarButton";
+
+import "./index.less";
+
+const propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  logoHref: PropTypes.string.isRequired,
+  title: PropTypes.node,
+};
+
+/**
+ * Global page-level header component.
+ */
+export class TopBar extends React.PureComponent {
+  static propTypes = propTypes;
+
+  static Button = TopBarButton;
+
+  render() {
+    const { children, className, title } = this.props;
+
+    return (
+      <FlexBox alignItems="center" className={classnames("dewey--TopBar", className)}>
+        <TopBarButton href={this.props.logoHref} className="dewey-TopBar--logoLink">
+          <Logo className="dewey--TopBar--logo" />
+        </TopBarButton>
+        {title && (
+          <h1 className="dewey--TopBar--title" title={title}>
+            {title}
+          </h1>
+        )}
+        <FlexBox alignItems="center" justify={Justify.END} grow>
+          {children}
+        </FlexBox>
+      </FlexBox>
+    );
+  }
+}
