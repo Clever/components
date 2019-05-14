cd36bc09b5394a60498989f3248483e233828182
Josh Lubaway <josh.lubaway@clever.com>
2019-05-14 16:19:24 -0700

    Cover existing ProgressBar labelType functionality

---
+++ test/ProgressBar_test.tsx
@@ -0,0 +1,22 @@
+import { mount } from "enzyme";
+import * as React from "react";
+
+import { ProgressBar } from "../src/";
+
+describe("ProgressBar Component", () => {
+  describe("Given a label type string", () => {
+    it("renders the provided label", () => {
+      const component = mount(<ProgressBar labelType="Foo Bar" showLabel="top-left" />);
+      expect(component.html()).toMatch("Foo Bar");
+    });
+  });
+
+  describe("Given a label type percentage and a fill number", () => {
+    it("renders the label as a fill percentage", () => {
+      const component = mount(
+        <ProgressBar labelType="percentage" fill={23} showLabel="top-left" />,
+      );
+      expect(component.html()).toMatch("23%");
+    });
+  });
+});
