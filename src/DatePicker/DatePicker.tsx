fc9e8ca3fe55144a9461d60079ef37739edb79aa
Renato <renato.prime@clever.com>
2019-07-08 01:03:50 -0700

    silence missing propType warnings

--- src/DatePicker/DatePicker.tsx
+++ src/DatePicker/DatePicker.tsx
@@ -1,5 +1,6 @@
 import * as classnames from "classnames";
 import * as React from "react";
+import * as PropTypes from "prop-types";
 import ReactDatePicker, { ReactDatePickerProps } from "react-datepicker";
 
 import "./DatePicker.less";
@@ -18,4 +19,6 @@ export default function DatePicker(props: Props) {
 
 DatePicker.propTypes = {
   ...ReactDatePicker.propTypes,
+  // This is incorrectly marked as required in ReactDatePicker.propTypes.
+  dropdownMode: PropTypes.oneOf(["scroll", "select"]),
 };
