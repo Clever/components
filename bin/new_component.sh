3c101ecf85fb29d8bb1fe3570e39ee971a2feba6
Ben Rollin <ben.rollin@clever.com>
2019-07-15 15:35:09 -0700

    Separate tsconfig for bin directory

--- bin/new_component.sh
+++ bin/new_component.sh
@@ -4,4 +4,4 @@
 cd "${BASH_SOURCE%/*}"
 
 NAME=$1
-../node_modules/.bin/ts-node --project ../tsconfig.json ./create_new_component.ts $NAME
+../node_modules/.bin/ts-node --project ../tsconfig.bin.json ./create_new_component.ts $NAME
