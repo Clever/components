6acc2d762b65f69d56b3e9f4d961f08d6428fdee
rzhang-clever <robert.zhang@clever.com>
2019-10-04 12:35:09 -0700

    type onClickCopy explicitly as arrow function rather that Function

--- src/CopyableInput/CopyableInput.tsx
+++ src/CopyableInput/CopyableInput.tsx
@@ -12,7 +12,7 @@ import "../less/forms.less";
 export interface Props extends TextInputProps {
   className?: string;
   enableCopy?: boolean;
-  onClickCopy?: Function;
+  onClickCopy?: () => void;
 }
 
 interface State {
