b1b891b612475437d9202556f45a6dd8e767a6f8
dengjulie <julie.deng@clever.com>
2020-05-14 15:34:00 -0700

    add secondary destructive button (#485)
    add secondary destructive button

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -35,6 +35,7 @@ const Type = {
   PRIMARY: "primary",
   SECONDARY: "secondary",
   DESTRUCTIVE: "destructive",
+  SECONDARY_DESTRUCTIVE: "secondaryDestructive",
   LINK: "link",
   LINK_PLAIN: "linkPlain",
   PLAIN: "plain",
@@ -99,7 +100,7 @@ export class Button extends React.PureComponent<Props> {
       propTypes,
     ) as (keyof typeof propTypes)[]);
 
-    if (type === Type.DESTRUCTIVE && size === Size.S) {
+    if ((type === Type.DESTRUCTIVE || type === Type.SECONDARY_DESTRUCTIVE) && size === Size.S) {
       throw new Error("Small destructive buttons are not supported");
     }
 
