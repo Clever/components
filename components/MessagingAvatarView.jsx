942a3817294233411692e8452ee14735349e3200
Adam Victor <adam.victor@clever.com>
2020-07-13 17:45:46 -0700

    Add docs

---
+++ components/MessagingAvatarView.jsx
@@ -0,0 +1,87 @@
+import * as React from "react";
+
+import Example, { CodeSample, ExampleCode } from "./Example";
+import PropDocumentation from "./PropDocumentation";
+import View from "./View";
+import Colors from "src/utils/Colors";
+import { MessagingAvatar, convertNameToInitials } from "src";
+
+import "./MessagingAvatarView.less";
+
+const cssClass = {
+  CONFIG_CONTAINER: "MessagingAvatarView--configContainer",
+  CONFIG_OPTIONS: "MessagingAvatarView--configOptions",
+  CONFIG: "MessagingAvatarView--config",
+  CONFIG_TOGGLE: "MessagingAvatarView--configToggle",
+  CONTAINER: "MessagingAvatarView",
+  INTRO: "MessagingAvatarView--intro",
+  PROPS: "MessagingAvatarView--props",
+};
+
+export default class MessagingAvatarView extends React.PureComponent {
+  static cssClass = cssClass;
+
+  render() {
+    return (
+      <View
+        className={cssClass.CONTAINER}
+        title="MessagingAvatar"
+        sourcePath="src/MessagingAvatar/MessagingAvatar.tsx"
+      >
+        <header className={cssClass.INTRO}>
+          <p>MessagingAvatar is to be used for displaying a user's avatar.</p>
+          <CodeSample>
+            {`
+              import {MessagingAvatar} from "clever-components";
+              // OR
+              import MessagingAvatar from "clever-components/dist/MessagingAvatar"; // Avoids importing all of clever-components.
+            `}
+          </CodeSample>
+        </header>
+
+        <Example title="Basic Usage:">
+          <ExampleCode>
+            <MessagingAvatar text={convertNameToInitials("Kristen Stark")} color={{color: Colors.PRIMARY_BLUE_TINT_2}} />
+            <br />
+            <MessagingAvatar text={convertNameToInitials("Kristen Stark")} color={{seed: "52c5e9d0e6ed0694212d69ed"}} />
+          </ExampleCode>
+          {this._renderConfig()}
+        </Example>
+
+        {this._renderProps()}
+      </View>
+    );
+  }
+
+  _renderConfig() {
+
+  }
+
+  // TODO: Update prop documentation.
+  _renderProps() {
+    return (
+      <PropDocumentation
+        title="<MessagingAvatar /> Props"
+        availableProps={[
+          {
+            name: "text",
+            type: "string",
+            description: "Text to display for the avatar's name",
+          },
+          {
+            name: "className",
+            type: "string",
+            description: "Optional additional CSS class name to apply to the container.",
+            optional: true,
+          },
+          {
+            name: "color",
+            type: "{color: string} | {seed: string}",
+            description: "An object containing either the color to use a seed for determining the color. If used, seed should be consistent for a given user.",
+          },
+        ]}
+        className={cssClass.PROPS}
+      />
+    );
+  }
+}
---
+++ components/MessagingInputView.jsx
@@ -0,0 +1,115 @@
+import * as React from "react";
+
+import Example, { CodeSample, ExampleCode } from "./Example";
+import PropDocumentation from "./PropDocumentation";
+import View from "./View";
+import { MessagingInput, FlexBox, ItemAlign } from "src";
+
+import "./MessagingInputView.less";
+
+const cssClass = {
+  CONFIG_CONTAINER: "MessagingInputView--configContainer",
+  CONFIG_OPTIONS: "MessagingInputView--configOptions",
+  CONFIG: "MessagingInputView--config",
+  CONFIG_TOGGLE: "MessagingInputView--configToggle",
+  CONTAINER: "MessagingInputView",
+  INTRO: "MessagingInputView--intro",
+  PROPS: "MessagingInputView--props",
+};
+
+export default class MessagingInputView extends React.PureComponent {
+  static cssClass = cssClass;
+
+  state = {
+    inputValue: "",
+  };
+
+  render() {
+    const { inputValue } = this.state;
+
+    return (
+      <View
+        className={cssClass.CONTAINER}
+        title="MessagingInput"
+        sourcePath="src/MessagingInput/MessagingInput.tsx"
+      >
+        <header className={cssClass.INTRO}>
+          <p>MessagingInputView is an input to be used for entering and submitting text for messaging.</p>
+          <CodeSample>
+            {`
+              import {MessagingInput} from "clever-components";
+              // OR
+              import MessagingInput from "clever-components/dist/MessagingInput"; // Avoids importing all of clever-components.
+            `}
+          </CodeSample>
+        </header>
+
+        <Example title="Basic Usage:">
+          <ExampleCode>
+            <MessagingInput
+              value={inputValue}
+              onChange={newValue => this.setState({ inputValue: newValue })}
+              recipientName="Demo Viewer"
+              onSubmit={message => {
+                // eslint-disable-next-line no-alert
+                alert(message);
+                this.setState({ inputValue: "" });
+              }}
+            />
+          </ExampleCode>
+        </Example>
+
+        {this._renderProps()}
+      </View>
+    );
+  }
+
+  _renderProps() {
+    return (
+      <PropDocumentation
+        title="<MessagingInput /> Props"
+        availableProps={[
+          {
+            name: "value",
+            type: "string",
+            description: "The text that's been entered in the MessagingInput.",
+          },
+          {
+            name: "recipientName",
+            type: "string",
+            description: "The name of the person that we're messaging.",
+          },
+          {
+            name: "onChange",
+            type: "(newValue: string) => void",
+            description: "Function that's called when the inputted text changes.",
+          },
+          {
+            name: "onSubmit",
+            type: "(message: string) => void",
+            description: "Function that's called when the text is submitted.",
+          },
+          {
+            name: "onBlur",
+            type: "() => void",
+            description: "Function that's called when the input is unfocused.",
+            optional: true,
+          },
+          {
+            name: "ref",
+            type: "React.Ref<TextArea>",
+            description: "Allows getting a ref to the underlying TextArea.",
+            optional: true,
+          },
+          {
+            name: "className",
+            type: "string",
+            description: "Optional additional CSS class name to apply to the container.",
+            optional: true,
+          },
+        ]}
+        className={cssClass.PROPS}
+      />
+    );
+  }
+}
