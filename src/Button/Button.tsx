af66f426a7078377e9c9698f1a885ea487d5e24a
Robert <47156875+rzhang-clever@users.noreply.github.com>
2020-08-25 12:44:43 -0700

    DISC-1795 Allow passing of aria-label to Button component (#510)
    * Move aria-label prop above additionalProps so that we don't set aria-label to null after setting it to something non-null
    
    * bump version
    
    * Button: adding ariaLabel to props
    
    Co-authored-by: wardmike <mike.ward@clever.com>

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -19,6 +19,7 @@ export interface Props {
   submit?: boolean;
   style?: React.CSSProperties;
   underlined?: boolean;
+  ariaLabel?: string;
   [additionalProp: string]: any;
 }
 
@@ -53,6 +54,7 @@ const propTypes = {
   submit: PropTypes.bool,
   style: PropTypes.object,
   underlined: PropTypes.bool,
+  ariaLabel: PropTypes.string,
 };
 
 const defaultProps = {
@@ -84,6 +86,7 @@ export class Button extends React.PureComponent<Props> {
 
   render() {
     const {
+      ariaLabel,
       className,
       disabled,
       href,
@@ -119,11 +122,14 @@ export class Button extends React.PureComponent<Props> {
       underlined ? "Button--linkUnderlined" : "",
     );
 
+    const aria = ariaLabel || (typeof value === "string" ? (value as string) : null);
+
     if (href == null || disabled) {
       // use <button>s for all disabled links and things with no href prop (buttons)
       return (
         <button
           {...additionalProps}
+          aria-label={aria}
           className={classes}
           disabled={disabled}
           onClick={onClick}
@@ -132,7 +138,6 @@ export class Button extends React.PureComponent<Props> {
           }}
           style={style}
           type={submit ? "submit" : "button"}
-          aria-label={typeof value === "string" ? (value as string) : null}
         >
           {value}
         </button>
@@ -141,6 +146,7 @@ export class Button extends React.PureComponent<Props> {
     return (
       <a
         {...additionalProps}
+        aria-label={aria}
         className={classes}
         href={href}
         onClick={onClick}
@@ -149,7 +155,6 @@ export class Button extends React.PureComponent<Props> {
         }}
         style={style}
         target={target}
-        aria-label={typeof value === "string" ? (value as string) : null}
       >
         {value}
       </a>
