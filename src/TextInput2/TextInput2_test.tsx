6bde5facbc69315df990e63899f904200239f851
John Huang <john.huang@clever.com>
2021-02-18 19:18:49 -0800

    basic tests

--- src/TextInput2/TextInput2_test.tsx
+++ src/TextInput2/TextInput2_test.tsx
@@ -1,33 +1,41 @@
 import * as React from "react";
 import { shallow } from "enzyme";
 
-import TextInput2 from "./TextInput2";
-import { Button } from "../Button/Button";
+import TextInput2, { cssClass } from "./TextInput2";
 
 describe("TextInput2", () => {
-  const { cssClass } = TextInput2;
-
-  const onPerformActionMock = jest.fn();
-
   afterEach(() => {
     jest.resetAllMocks();
   });
 
   it("renders", () => {
     const myComponent = shallow(
-      <TextInput2 onPerformAction={onPerformActionMock}>Test Content</TextInput2>,
+      <TextInput2
+        name="TextInput2--name"
+        label="testLabel"
+        value="testValue"
+        onChange={() => undefined}
+      >
+        Test Content
+      </TextInput2>,
     );
 
-    // TODO: Verify basic structure/contents.
     expect(myComponent.props().className).toMatch(cssClass.CONTAINER);
-    expect(myComponent).toIncludeText("Test Content");
-    expect(myComponent.find(`.${cssClass.SUB_ELEMENT}`)).toExist();
-    expect(myComponent.find(Button)).toExist();
+    expect(myComponent).toIncludeText("testLabel");
+    expect(myComponent.find(`.${cssClass.INPUT}`)).toExist();
+    expect(myComponent.find("input").props().value).toBe("testValue");
+    expect(myComponent.find("input")).toExist();
   });
 
   it("supports additional class names", () => {
     const myComponent = shallow(
-      <TextInput2 className="my--custom--class" onPerformAction={onPerformActionMock}>
+      <TextInput2
+        className="my--custom--class"
+        name="TextInput2--name"
+        label="testLabel"
+        value="testValue"
+        onChange={() => undefined}
+      >
         Test Content
       </TextInput2>,
     );
@@ -37,6 +45,7 @@ describe("TextInput2", () => {
   });
 
   // TODO: Test any relevant state changes/event handling/prop-driven rendering.
+  /*
   it("propagates event", () => {
     const myComponent = shallow(
       <TextInput2 onPerformAction={onPerformActionMock}>Test Content</TextInput2>,
@@ -45,4 +54,5 @@ describe("TextInput2", () => {
     myComponent.find(Button).simulate("click");
     expect(onPerformActionMock).toHaveBeenCalledWith("action performed");
   });
+  */
 });
