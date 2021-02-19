b5dbe309daa9ea7b8139736740ea012ae328997a
John Huang <john.huang@clever.com>
2021-02-18 18:44:46 -0800

    basic implementation for TextInput2

--- src/TextInput2/TextInput2.tsx
+++ src/TextInput2/TextInput2.tsx
@@ -1,54 +1,101 @@
 import * as classnames from "classnames";
 import * as React from "react";
+import { useState } from "react";
 
-import { Button } from "../Button/Button";
+import { FormElementSize, formElementSizeClassName } from "../utils/Forms";
+import { Values } from "../utils/types";
 
 import "./TextInput2.less";
 
-export interface Props {
-  children: React.ReactNode;
-  className?: string;
-  onPerformAction: (arg?: string) => any;
-}
+export const TextInput2Requirement = {
+  OPTIONAL: "optional",
+  REQUIRED: "required",
+} as const;
 
-// TODO: Uncomment if this component is stateful, and pass State to PureComponent. Remove otherwise.
-// interface State {
-//   sampleState1: boolean;
-//   sampleState2: string;
-// }
+export interface Props {
+  className?: string;
+  name: string;
+  label: React.ReactNode;
+  placeholder?: string;
+  helpText?: React.ReactNode;
+  icon?: React.ReactNode;
+  requirement?: Values<typeof TextInput2Requirement>;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  size?: Values<typeof FormElementSize>;
+}
 
 export const cssClass = {
-  CONTAINER: "TextInput2",
-  SUB_ELEMENT: "TextInput2--subElement",
+  CONTAINER: "TextInput2--container",
+  INFO_ROW: "TextInput2--infoRow",
+  INFO_REQUIREMENT: "TextInput2--infoRequirement",
+  LABEL: "TextInput2--label",
+  LEADING_ICON: "TextInput2--leadingIcon",
+  INPUT_CONTAINER: "TextInput2--inputContainer",
+  INPUT: "TextInput2--input",
+  INPUT_FOCUSED: "TextInput2--input--focused",
+  HELP_TEXT: "TextInput2--helpText",
 };
 
-/**
- * TODO: Add short description.
+/*
+ * text input
  */
-export class TextInput2 extends React.PureComponent<Props> {
-  // TODO: Uncomment if this component is stateful. Remove otherwise.
-  // state = {
-  //   sampleState1: false,
-  //   sampleState2: "foo",
-  // };
+export const TextInput2: React.FC<Props> = ({
+  className,
+  name,
+  label,
+  placeholder,
+  helpText,
+  icon,
+  requirement,
+  value,
+  onChange,
+  size,
+}) => {
+  const id = name;
+  const [isFocused, setIsFocused] = useState(false);
 
-  render() {
-    const { children, className } = this.props;
-
-    return (
-      <div className={classnames(cssClass.CONTAINER, className)}>
-        <h3>TODO</h3>
-        <p>{children}</p>
-        <div className={cssClass.SUB_ELEMENT}>
-          <Button
-            onClick={this._handlePerformAction}
-            type={Button.Type.PRIMARY}
-            value="Do something"
-          />
+  return (
+    <div className={classnames(cssClass.CONTAINER, formElementSizeClassName(size), className)}>
+      {(!!requirement || !!label) && (
+        <div className={cssClass.INFO_ROW}>
+          {!!label ? (
+            <label className={cssClass.LABEL} htmlFor={id}>
+              {label}
+            </label>
+          ) : (
+            // placeholder for flexbox
+            <div></div>
+          )}
+          {!!requirement && (
+            <span className={cssClass.INFO_REQUIREMENT} aria-live="polite">
+              {requirement}
+            </span>
+          )}
         </div>
+      )}
+      <div className={classnames(cssClass.INPUT_CONTAINER, isFocused && cssClass.INPUT_FOCUSED)}>
+        {icon && <i className={cssClass.LEADING_ICON}>{icon}</i>}
+        <input
+          id={id}
+          name={id}
+          className={cssClass.INPUT}
+          role="textbox"
+          type={"text"}
+          value={value}
+          placeholder={placeholder}
+          onChange={onChange}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
       </div>
-    );
-  }
+      {!!helpText && <div className={cssClass.HELP_TEXT}>{helpText}</div>}
+    </div>
+  );
+};
 
-  _handlePerformAction = () => this.props.onPerformAction("action performed");
-}
+TextInput2.defaultProps = {
+  size: FormElementSize.FULL_WIDTH,
+};
+
+
