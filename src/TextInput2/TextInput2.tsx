f3b1e5867431b38aa16dd689a99e1044d7eb8379
johnhuangclever <john.huang@clever.com>
2021-02-22 10:28:03 -0800

    ID-48 custom errors disabled (#589)
    * disabled requirement state
    
    * error messaging and custom validation
    
    * update docs
    
    * 2.73.0

--- src/TextInput2/TextInput2.tsx
+++ src/TextInput2/TextInput2.tsx
@@ -12,6 +12,7 @@ import "./TextInput2.less";
 export const TextInput2Requirement = {
   OPTIONAL: "optional",
   REQUIRED: "required",
+  DISABLED: "disabled",
 } as const;
 
 export interface Props {
@@ -26,6 +27,8 @@ export interface Props {
   requirement?: Values<typeof TextInput2Requirement>;
   obscurable?: boolean;
   initialIsInError?: boolean;
+  // returns an error message, null for no error
+  errorValidation?: (value: string) => string | null;
   value: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   size?: Values<typeof FormElementSize>;
@@ -42,8 +45,10 @@ export const cssClass = {
   INPUT_CONTAINER: "TextInput2--inputContainer",
   INPUT_CONTAINER_FOCUSED: "TextInput2--inputContainer--focused",
   INPUT_CONTAINER_ERROR: "TextInput2--inputContainer--error",
+  INPUT_CONTAINER_DISABLED: "TextInput2--inputContainer--disabled",
   INPUT: "TextInput2--input",
-  HELP_TEXT: "TextInput2--helpText",
+  FOOTER_TEXT: "TextInput2--footerText",
+  ERROR_MESSAGE: "TextInput2--footerText--error",
   ERROR_ICON: "TextInput2--errorIcon",
   HIDE_SHOW: "TextInput2--hideShowButton",
 };
@@ -61,6 +66,7 @@ const TextInput2: React.FC<Props> = ({
   icon,
   requirement,
   initialIsInError,
+  errorValidation,
   obscurable,
   value,
   onChange,
@@ -72,26 +78,33 @@ const TextInput2: React.FC<Props> = ({
   const [isObscured, setIsObscured] = useState(true);
   const inputType = obscurable && isObscured ? "password" : "text";
 
-  const [isInError, setIsInError] = useState(initialIsInError);
+  // empty string is an error state with no message (e.g. required)
+  const [errorMessage, setErrorMessage] = useState(initialIsInError ? "" : null);
 
   useEffect(() => {
     if (requirement === TextInput2Requirement.REQUIRED && value === "") {
-      setIsInError(initialIsInError);
+      setErrorMessage(initialIsInError ? "" : null);
     }
   }, [initialIsInError]);
 
   useEffect(() => {
     // don't show error if nothing has happened yet
-    if (!isFocused && !isInError) {
+    if (!isFocused && errorMessage === null) {
       return;
     }
 
     if (requirement === TextInput2Requirement.REQUIRED && value === "") {
-      setIsInError(true);
+      setErrorMessage("");
       return;
     }
 
-    setIsInError(false);
+    const newErrorMessage = errorValidation(value);
+    if (newErrorMessage) {
+      setErrorMessage(newErrorMessage);
+      return;
+    }
+
+    setErrorMessage(null);
   }, [value, isFocused]);
 
   return (
@@ -104,7 +117,7 @@ const TextInput2: React.FC<Props> = ({
           >
             {label}
           </label>
-          {!!requirement && (
+          {requirement && (
             <label className={cssClass.INFO_REQUIREMENT} aria-live="polite" htmlFor={id}>
               {requirement}
             </label>
@@ -115,7 +128,8 @@ const TextInput2: React.FC<Props> = ({
         className={classnames(
           cssClass.INPUT_CONTAINER,
           isFocused && cssClass.INPUT_CONTAINER_FOCUSED,
-          isInError && cssClass.INPUT_CONTAINER_ERROR,
+          errorMessage != null && cssClass.INPUT_CONTAINER_ERROR,
+          requirement === TextInput2Requirement.DISABLED && cssClass.INPUT_CONTAINER_DISABLED,
         )}
       >
         {icon && <i className={cssClass.LEADING_ICON}>{icon}</i>}
@@ -127,11 +141,14 @@ const TextInput2: React.FC<Props> = ({
           type={inputType}
           value={value}
           placeholder={placeholder}
+          disabled={requirement === TextInput2Requirement.DISABLED}
           onChange={onChange}
           onFocus={() => setIsFocused(true)}
           onBlur={() => setIsFocused(false)}
         />
-        {isInError && <FontAwesome className={cssClass.ERROR_ICON} name="exclamation-circle" />}
+        {errorMessage != null && (
+          <FontAwesome className={cssClass.ERROR_ICON} name="exclamation-circle" />
+        )}
         {obscurable && (
           <Button
             className={cssClass.HIDE_SHOW}
@@ -141,13 +158,18 @@ const TextInput2: React.FC<Props> = ({
           />
         )}
       </div>
-      {!!helpText && <div className={cssClass.HELP_TEXT}>{helpText}</div>}
+      {(errorMessage || helpText) && (
+        <div className={classnames(cssClass.FOOTER_TEXT, errorMessage && cssClass.ERROR_MESSAGE)}>
+          {errorMessage || helpText}
+        </div>
+      )}
     </div>
   );
 };
 
 TextInput2.defaultProps = {
   initialIsInError: false,
+  errorValidation: () => null,
   size: FormElementSize.FULL_WIDTH,
 };
 
