7328d321b8ddf6caa103b888ff4b8f467aee96f1
jonjonray <jonjonray@gmail.com>
2021-04-13 18:22:58 -0400

    handle line breaks in QuotedAnnouncementBubble

--- src/AnnouncementBubble/QuotedAnnouncementBubble.tsx
+++ src/AnnouncementBubble/QuotedAnnouncementBubble.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { useState } from "react";
 import * as cx from "classnames";
 import Linkify from "react-linkify";
+import * as _ from "lodash";
 import { FlexBox, Button } from "../";
 import { formatDateForTimestamp } from "./NormalAnnouncementBubble";
 import { componentDecorator, matchDecorator } from "../MessagingBubble/linkifyUtils";
@@ -34,11 +35,26 @@ export const QuotedAnnouncementBubble: React.FC<Props> = ({
 }: Props) => {
   const [isExpanded, setisExpanded] = useState(false);
 
-  // If the content is not expanded, limit the message length so that screen readers
-  // don't attempt to read the entire hidden message. This only works for plain-string children
+  /* If the content is not expanded, limit the message length so that screen readers
+    don't attempt to read the entire hidden message. This only works for plain-string children */
   let content = children;
   if (!isExpanded && typeof content === "string") {
-    content = content.substring(0, 300);
+    let previewLength = 100;
+    let firstLineBreak = content.indexOf("\n");
+    /* If we have a line break and the first line of text is shorter than the preview length
+     and another line in the body of text, we should preserve the line break in the preview 
+     and adjust the preview width accordingly */
+    if (firstLineBreak > -1 && firstLineBreak < previewLength - 1) {
+      const allLines = content.split("\n");
+      const firstLine = allLines[0];
+      const longestLine = allLines.reduce((longestLine, line) =>
+        longestLine.length > line.length ? longestLine : line,
+      );
+
+      content = _.padEnd(firstLine, longestLine.length, "\xa0");
+    } else {
+      content = content.substring(0, previewLength);
+    }
   }
 
   const buttonText = isExpanded ? "Show less" : "Show more";
