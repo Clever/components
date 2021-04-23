85d4cf83434b0729a25f7abbaa593f6bce6a7a43
Vamsi Chunduru <vamsi.chunduru@clever.com>
2021-04-22 17:31:56 -0700

    Allow Menu.Item to be disabled

--- components/MenuView.jsx
+++ components/MenuView.jsx
@@ -205,6 +205,9 @@ export default class MenuView extends React.PureComponent {
               <Menu.Item href="https://google.com/search?q=four" target="_blank">
                 Plain Link Menu Item
               </Menu.Item>
+              <Menu.Item disabled>
+                Disabled Item
+              </Menu.Item>
             </Menu>
           </ExampleCode>
           {this._renderConfig()}
