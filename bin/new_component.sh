9f1ff769317131bc18514f2a5cc90fc74fb50ec0
John Huang <john.huang@clever.com>
2021-05-05 10:35:45 -0700

    remove dry-run from jscodeshift
    it wasn't doing anything anyways, the proper dry run flag is -d or --dry

--- bin/new_component.sh
+++ bin/new_component.sh
@@ -5,9 +5,9 @@ cd "${BASH_SOURCE%/*}"
 
 NAME=$1
 ../node_modules/.bin/ts-node --project ../tsconfig.bin.json ./create_new_component.ts $NAME
-npx jscodeshift --dry-run --componentName="$NAME" -t ./addToSidebar.ts ../docs/components/SideBar/SideBar.jsx
+npx jscodeshift --componentName="$NAME" -t ./addToSidebar.ts ../docs/components/SideBar/SideBar.jsx
 ../node_modules/.bin/prettier --write ../docs/components/SideBar/SideBar.jsx
-npx jscodeshift --dry-run --componentName="$NAME" -t ./addRoute.ts ../docs/docs.jsx
+npx jscodeshift --componentName="$NAME" -t ./addRoute.ts ../docs/docs.jsx
 ../node_modules/.bin/prettier --write ../docs/docs.jsx
-npx jscodeshift --dry-run --componentName="$NAME" -t ./addScreenshot.ts ../docs/components/ComponentsView.jsx
+npx jscodeshift --componentName="$NAME" -t ./addScreenshot.ts ../docs/components/ComponentsView.jsx
 ../node_modules/.bin/prettier --write ../docs/components/ComponentsView.jsx
