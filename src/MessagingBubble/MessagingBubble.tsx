a1b07f96f22543c425bcbef1ddf2e6f6f6673e7c
Cory Fauver <cory.fauver@clever.com>
2021-06-07 10:07:57 -0700

    adds timestamp & its styling to the message bubble

--- src/MessagingBubble/MessagingBubble.tsx
+++ src/MessagingBubble/MessagingBubble.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as moment from "moment";
 import Linkify from "react-linkify";
 import * as cx from "classnames";
 import FlexBox from "../flex/FlexBox";
@@ -10,9 +11,15 @@ const cssClasses = {
   MESSAGE_BASE: "MessagingBubble--Message",
   MESSAGE_OWN: "MessagingBubble--Message--Own",
   MESSAGE_OTHER: "MessagingBubble--Message--Other",
+  MESSAGE_CONTAINER_OWN: "MessagingBubble--Message--Container--Own",
+  MESSAGE_CONTAINER_OTHER: "MessagingBubble--Message--Container--Other",
   MESSAGE_REPLY_OWN: "MessagingBubble--Message--Reply--Own",
   MESSAGE_REPLY_OTHER: "MessagingBubble--Message--Reply--Other",
   MESSAGE_REPLY_PARENT: "MessagingBubble--Message--Reply--Parent",
+  MESSAGE_TIME_BUBBLE_CONTAINER_OWN: "MessagingBubble--Message--TimestampBubbleContainer--Own",
+  MESSAGE_TIME_BUBBLE_CONTAINER_OTHER: "MessagingBubble--Message--TimestampBubbleContainer--Other",
+  MESSAGE_TIMESTAMP_OWN: "MessagingBubble--Message--Timestamp--Own",
+  MESSAGE_TIMESTAMP_OTHER: "MessagingBubble--Message--Timestamp--Other",
   MESSAGE_ATTACHMENT_OWN: "MessagingBubble--Message--Attachment--Own",
   MESSAGE_ATTACHMENT_OTHER: "MessagingBubble--Message--Attachment--Other",
 };
@@ -20,19 +27,33 @@ const cssClasses = {
 interface Props {
   className?: string;
   children: React.ReactNode;
+  timestamp: Date;
   replyTo?: React.ReactNode;
   attachments?: React.ReactNode[];
   theme: "ownMessage" | "otherMessage";
 }
 
+// Helper function: Format a Date for our pretty timestamps.
+//  Always returns "xx:xx <AM/PM>" format.
+function _formatDateForTimestamp(date: Date): string {
+  return moment(date).format("h:mm A");
+}
+
 export const MessagingBubble: React.FC<Props> = ({
   className,
   children,
+  timestamp,
   theme,
   replyTo,
   attachments,
 }: Props) => {
   const isOwnMessage = theme === "ownMessage";
+
+  const containerClassNames = cx(
+    className,
+    isOwnMessage ? cssClasses.MESSAGE_CONTAINER_OWN : cssClasses.MESSAGE_CONTAINER_OTHER,
+  );
+  
   const bubbleClassNames = cx(
     cssClasses.MESSAGE_BASE,
     isOwnMessage ? cssClasses.MESSAGE_OWN : cssClasses.MESSAGE_OTHER,
@@ -44,20 +65,34 @@ export const MessagingBubble: React.FC<Props> = ({
     isOwnMessage ? cssClasses.MESSAGE_REPLY_OWN : cssClasses.MESSAGE_REPLY_OTHER,
   );
 
+  const timeAndBubbleContainerClasses = cx(
+    isOwnMessage ? cssClasses.MESSAGE_TIME_BUBBLE_CONTAINER_OWN : cssClasses.MESSAGE_TIME_BUBBLE_CONTAINER_OTHER,
+  );
+
+  const timestampClassNames = cx(
+    cssClasses.MESSAGE_BASE,
+    isOwnMessage ? cssClasses.MESSAGE_TIMESTAMP_OWN : cssClasses.MESSAGE_TIMESTAMP_OTHER,
+  );
+
   const attachmentClassNames = cx(
     cssClasses.MESSAGE_BASE,
     isOwnMessage ? cssClasses.MESSAGE_ATTACHMENT_OWN : cssClasses.MESSAGE_ATTACHMENT_OTHER,
   );
 
   return (
-    <div className={className}>
-      <div className={bubbleClassNames}>
-        {replyTo && <div className={replyClassNames}>{replyTo}</div>}
-        <Linkify componentDecorator={componentDecorator} matchDecorator={matchDecorator}>
-          {children}
-        </Linkify>
-      </div>
+    <FlexBox column className={containerClassNames}>
+      <FlexBox className={timeAndBubbleContainerClasses}>
+        <span className={timestampClassNames}>
+          {_formatDateForTimestamp(timestamp)}
+        </span>
+        <div className={bubbleClassNames}>
+          {replyTo && <div className={replyClassNames}>{replyTo}</div>}
+          <Linkify componentDecorator={componentDecorator} matchDecorator={matchDecorator}>
+            {children}
+          </Linkify>
+        </div>
+      </FlexBox>
       {attachments && <FlexBox className={attachmentClassNames}>{attachments}</FlexBox>}
-    </div>
+    </FlexBox>
   );
 };
