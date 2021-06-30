73941c6f0e52d4c7f2d33ee0c5f3b1529dd2c42b
Jonah Schwartz <jonah.schwartz@clever.com>
2021-06-30 16:18:52 -0700

    cleanup AttachmentPreview docs

--- docs/components/AttachmentPreviewView.jsx
+++ docs/components/AttachmentPreviewView.jsx
@@ -3,17 +3,11 @@ import * as React from "react";
 import Example, { CodeSample, ExampleCode } from "./Example";
 import PropDocumentation from "./PropDocumentation";
 import View from "./View";
-import { AttachmentPreview, FlexBox, ItemAlign, SegmentedControl } from "src";
+import { AttachmentPreview, Button } from "src";
 
 import "./AttachmentPreviewView.less";
-import { Button } from "../../src";
 
 const cssClass = {
-  CONFIG_CONTAINER: "AttachmentPreviewView--configContainer",
-  CONFIG_OPTIONS: "AttachmentPreviewView--configOptions",
-  CONFIG: "AttachmentPreviewView--config",
-  CONFIG_LABEL_TEXT: "TextInput2View--configLabelText",
-  CONFIG_TOGGLE: "AttachmentPreviewView--configToggle",
   CONTAINER: "AttachmentPreviewView",
   INTRO: "AttachmentPreviewView--intro",
   PROPS: "AttachmentPreviewView--props",
@@ -23,16 +17,14 @@ export default class AttachmentPreviewView extends React.PureComponent {
   static cssClass = cssClass;
 
   state = {
-    multiOption1: "small",
-    optionToggle1: false,
-    optionToggle2: true,
     showingPreview: false,
   };
 
   render() {
-    const { optionToggle1, showingPreview } = this.state;
+    const { showingPreview } = this.state;
 
     const file = {
+      id: "12345",
       title: "Raccooooooon.jpg",
       fileType: "jpg",
       url:
@@ -46,8 +38,11 @@ export default class AttachmentPreviewView extends React.PureComponent {
         sourcePath="src/AttachmentPreview/AttachmentPreview.tsx"
       >
         <header className={cssClass.INTRO}>
-          <p>TODO: Describe your component/state it's purpose</p>
-          <p>TODO(optional): Describe scenarios where the component might be useful.</p>
+          <p>A full page preview for messaging attachments.</p>
+          <p>
+            Currently only used for images, but eventually will be used for PDFs and other
+            attachment types.
+          </p>
           <CodeSample>
             {`
               import {AttachmentPreview} from "clever-components";
@@ -62,19 +57,15 @@ export default class AttachmentPreviewView extends React.PureComponent {
             <Button onClick={() => this.setState({ showingPreview: true })}>Show preview</Button>
             {showingPreview && (
               <AttachmentPreview
+                attachmentID={file.id}
                 attachmentName={file.title}
                 attachmentURL={file.url}
                 fileType={file.fileType}
                 onClickDownload={() => console.log("Downloaded attachment!")}
-                onClose={(e) => {
-                  this.setState({ showingPreview: false });
-                }}
-              >
-                {optionToggle1 ? "Something changed 🤔" : "My custom content."}
-              </AttachmentPreview>
+                onClose={() => this.setState({ showingPreview: false })}
+              />
             )}
           </ExampleCode>
-          {this._renderConfig()}
         </Example>
 
         {this._renderProps()}
@@ -82,68 +73,60 @@ export default class AttachmentPreviewView extends React.PureComponent {
     );
   }
 
-  // TODO: Update or remove config options.
-  _renderConfig() {
-    const { multiOption1, optionToggle1, optionToggle2 } = this.state;
-
-    return (
-      <FlexBox alignItems={ItemAlign.CENTER} className={cssClass.CONFIG_CONTAINER} wrap>
-        <div className={cssClass.CONFIG}>
-          Size:
-          <SegmentedControl
-            className={cssClass.CONFIG_OPTIONS}
-            onSelect={(value) => this.setState({ multiOption1: value })}
-            options={[
-              { content: "Small", value: "small" },
-              { content: "Medium", value: "medium" },
-              { content: "Large", value: "large" },
-            ]}
-            value={multiOption1}
-          />
-        </div>
-        <label className={cssClass.CONFIG}>
-          <input
-            type="checkbox"
-            checked={optionToggle1}
-            className={cssClass.CONFIG_TOGGLE}
-            onChange={(e) => this.setState({ optionToggle1: e.target.checked })}
-          />{" "}
-          <span className={cssClass.CONFIG_LABEL_TEXT}>Option Toggle 1</span>
-        </label>
-        <label className={cssClass.CONFIG}>
-          <input
-            type="checkbox"
-            checked={optionToggle2}
-            className={cssClass.CONFIG_TOGGLE}
-            onChange={(e) => this.setState({ optionToggle2: e.target.checked })}
-          />{" "}
-          <span className={cssClass.CONFIG_LABEL_TEXT}>Option Toggle 2</span>
-        </label>
-      </FlexBox>
-    );
-  }
-
-  // TODO: Update prop documentation.
   _renderProps() {
     return (
       <PropDocumentation
         title="<AttachmentPreview /> Props"
         availableProps={[
           {
-            name: "children",
-            type: "React.Node",
-            description: "AttachmentPreview content.",
+            name: "attachmentID",
+            type: "string",
+            description: "Unique ID for a given attachment",
+          },
+          {
+            name: "attachmentName",
+            type: "string",
+            description: "The name of the attachment file",
+          },
+          {
+            name: "attachmentURL",
+            type: "string",
+            description: "URL for the attachment",
           },
           {
             name: "className",
             type: "string",
-            description: "Optional additional CSS class name to apply to the container.",
+            description: "Optional class name",
             optional: true,
           },
           {
-            name: "onPerformAction",
-            type: "Function",
-            description: "Handler function for the performAction event.",
+            name: "closeButtonAriaLabel",
+            type: "string",
+            description: "Optional ARIA label for the close button",
+            defaultValue: "close attachment preview",
+            optional: true,
+          },
+          {
+            name: "downloadButtonText",
+            type: "string",
+            description: "Optional text for the download button",
+            defaultValue: "Download",
+            optional: true,
+          },
+          {
+            name: "fileType",
+            type: "AttachmentFileType",
+            description: "FileType for the attachment",
+          },
+          {
+            name: "onClickDownload",
+            type: "(attachmentID: string) => void",
+            description: "Function to run on clicking the download button",
+          },
+          {
+            name: "onClose",
+            type: "() => void",
+            description: "function to run on clicking the close button",
           },
         ]}
         className={cssClass.PROPS}
