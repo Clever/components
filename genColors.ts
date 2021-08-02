2ff881e6061f25f44bbc2025dc887a2ae62bd82d
Renato Prime <prime@clever.com>
2021-08-02 11:10:40 -0700

    define a new red color variable 🔴

--- genColors.ts
+++ genColors.ts
@@ -51,6 +51,7 @@ const Colors = {
     ALERT_RED_SHADE_2: "#AE121F",
     ALERT_RED_TINT_1: "#ED4B59",
     ALERT_RED_TINT_2: "#F56A75",
+    ALERT_RED_SHADE: "#B30C10",
   },
 
   // Secondary colors:
