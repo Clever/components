f07a7c930bbc6548182426dcd31fc37422444065
Jonah Schwartz <jonah.schwartz@clever.com>
2021-08-25 16:48:34 -0700

    add dropzone container classname

--- src/FileInput/FileInput.tsx
+++ src/FileInput/FileInput.tsx
@@ -169,7 +169,7 @@ export class FileInput extends React.Component<Props, State> {
     return (
       <Dropzone
         accept={this.props.accept}
-        className={this.props.dropzoneClass}
+        className={classnames("DropzoneContainer", this.props.dropzoneClass)}
         style={dropzoneStyle}
         multiple={false}
         onDropAccepted={this.onDropAccepted}
