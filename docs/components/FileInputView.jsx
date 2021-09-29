11eaa02f81fe92a17b08ee595b216a2e2dce6b58
Renato Prime <prime@clever.com>
2021-09-29 14:45:58 -0700

    fix typo

--- docs/components/FileInputView.jsx
+++ docs/components/FileInputView.jsx
@@ -142,8 +142,7 @@ export default class FileInputView extends React.PureComponent {
             {
               name: "store",
               type: "Function",
-              description:
-                "Function to be called when the user has seleceted a file to be uploaded",
+              description: "Function to be called when the user has selected a file to be uploaded",
               defaultValue: "False",
             },
             {
