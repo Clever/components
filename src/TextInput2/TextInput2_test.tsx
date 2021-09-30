2733131a6ee5bd928ce5dd008e2bb4a0675ecaa9
antoniodangond <antoniodangond@gmail.com>
2021-09-29 18:06:32 -0700

    Modifies TextInput2 error validation behavior
    Changes initialIsInError prop to errorOnEmpty, to more accurately describe the prop's use case. If errorOnEmpty, empty error state is displayed if the input has no value and is out of focus (the error state is removed on focus). If not errorOnEmpty, error state is only displayed on blur, and is removed if the value is empty or if the error is resolved.

--- src/TextInput2/TextInput2_test.tsx
+++ src/TextInput2/TextInput2_test.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 import TextInput2, { cssClass } from "./TextInput2";
+
+// util functions
+const getInput = (wrapper) => wrapper.find("input");
+const getErrorContainer = (wrapper) => wrapper.find(`.${cssClass.INPUT_CONTAINER_ERROR}`);
+const changeInputValue = (input, value) => {
+  input.simulate("change", { target: { value } });
+};
 
 describe("TextInput2", () => {
   let defaultProps;
@@ -54,18 +61,105 @@ describe("TextInput2", () => {
     expect(myComponent.props().className).toMatch("my--custom--class");
   });
 
+  describe("error validation", () => {
+    const validValue = "valid";
+    const invalidValue = "invalid";
+    let wrapper;
+    beforeEach(() => {
+      defaultProps = {
+        ...defaultProps,
+        errorValidation: (value) => (value === invalidValue ? "test error message" : null),
+        onChange: (e) => {
+          wrapper.setProps({ value: e.target.value });
+          wrapper.update();
+        },
+      };
+    });
+
+    it("displays error on blur if provided errorValidation function returns a string", () => {
+      wrapper = mount(<TextInput2 {...defaultProps} />);
+      const input = getInput(wrapper);
+
+      input.simulate("focus");
+      changeInputValue(input, invalidValue);
+      let errorContainer = getErrorContainer(wrapper);
+      // error should not be displayed while element is still in focus
+      expect(errorContainer).not.toExist();
+
+      input.simulate("blur");
+      errorContainer = getErrorContainer(wrapper);
+      // error is displayed after focus is removed
+      expect(errorContainer).toExist();
+    });
+
+    it("removes error while focused if provided errorValidation function returns null", () => {
+      defaultProps.value = invalidValue;
+      wrapper = mount(<TextInput2 {...defaultProps} />);
+      let errorContainer = getErrorContainer(wrapper);
+      // error is already present from invalid value
+      expect(errorContainer).toExist();
+
+      const input = getInput(wrapper);
+      input.simulate("focus");
+      changeInputValue(input, validValue);
+      errorContainer = getErrorContainer(wrapper);
+      // error is removed after valid value is set
+      expect(errorContainer).not.toExist();
+    });
+
+    it("removes error if input value is empty string", () => {
+      defaultProps.value = invalidValue;
+      wrapper = mount(<TextInput2 {...defaultProps} />);
+      let errorContainer = getErrorContainer(wrapper);
+      // error is already present from invalid value
+      expect(errorContainer).toExist();
+
+      const input = getInput(wrapper);
+      input.simulate("focus");
+      changeInputValue(input, "");
+      errorContainer = getErrorContainer(wrapper);
+      // error is removed after value is set to empty string
+      expect(errorContainer).not.toExist();
+    });
+
+    it("displays error if value is empty string and errorOnEmpty is true", () => {
+      defaultProps.errorOnEmpty = true;
+      defaultProps.value = "";
+      wrapper = mount(<TextInput2 {...defaultProps} />);
+      const errorContainer = getErrorContainer(wrapper);
+      // error is displayed for empty value
+      expect(errorContainer).toExist();
+    });
+
+    it("removes error on focus if value is empty string and errorOnEmpty is true", () => {
+      defaultProps.errorOnEmpty = true;
+      defaultProps.value = "";
+      wrapper = mount(<TextInput2 {...defaultProps} />);
+      const input = getInput(wrapper);
+
+      let errorContainer = getErrorContainer(wrapper);
+      // error is displayed for empty value
+      expect(errorContainer).toExist();
+
+      input.simulate("focus");
+      errorContainer = getErrorContainer(wrapper);
+      // empty error is removed on focus
+      expect(errorContainer).not.toExist();
+    });
+  });
+
   describe("event handling", () => {
     it("executes onFocus prop if provided", () => {
       defaultProps.onFocus = jest.fn();
       const wrapper = shallow(<TextInput2 {...defaultProps} />);
-      wrapper.find("input").simulate("focus");
+      getInput(wrapper).simulate("focus");
       expect(defaultProps.onFocus).toHaveBeenCalled();
     });
 
     it("executes onBlur prop if provided", () => {
       defaultProps.onBlur = jest.fn();
       const wrapper = shallow(<TextInput2 {...defaultProps} />);
-      wrapper.find("input").simulate("blur");
+      getInput(wrapper).simulate("blur");
       expect(defaultProps.onBlur).toHaveBeenCalled();
     });
   });
