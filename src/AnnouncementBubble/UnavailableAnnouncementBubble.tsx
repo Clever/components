f9584410cf9b793b69897844314e9137c837e233
Chloe Caelynn <chloe.caelynn@clever.com>
2022-04-01 14:33:05 -0700

    PRTL-1749 - Add UnavailableAnnouncementBubble component variant, used when a user no longer has access to an announcement which they replied to in a DM.

---
+++ src/AnnouncementBubble/UnavailableAnnouncementBubble.tsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+import * as cx from "classnames";
+import { FlexBox } from "../";
+
+import "./UnavailableAnnouncementBubble.less";
+
+type ColorTheme = "white" | "light" | "dark";
+
+function cssClass(element: string) {
+  return `UnavailableAnnouncementBubble--${element}`;
+}
+
+function cssClassWithColorTheme(element: string, colorTheme: ColorTheme) {
+  return [
+    `UnavailableAnnouncementBubble--${element}`,
+    `UnavailableAnnouncementBubble--${element}--${colorTheme}`,
+  ];
+}
+
+export interface Props {
+  bubbleType: "unavailable";
+
+  className?: string;
+  colorTheme: ColorTheme;
+
+  // Temporary props to allow overriding text with translations
+  unavailableNoticeHeader: string;
+  unavailableNoticeText: string;
+}
+
+export const UnavailableAnnouncementBubble: React.FC<Props> = ({
+  className,
+  colorTheme,
+  unavailableNoticeHeader,
+  unavailableNoticeText,
+}: Props) => {
+  const iconName =
+    colorTheme === "dark"
+      ? "megaphone-slash-darkColorTheme.svg"
+      : "megaphone-slash-lightColorTheme.svg";
+
+  return (
+    <FlexBox
+      grow
+      alignItems="center"
+      justify="start"
+      className={cx(cssClassWithColorTheme("container", colorTheme), className)}
+    >
+      <img alt="" className={cssClass("icon")} src={require(`./${iconName}`)} />
+      <div className={cssClass("textContainer")}>
+        <div className={cx(cssClassWithColorTheme("header", colorTheme))}>
+          {unavailableNoticeHeader}
+        </div>
+        <div className={cx(cssClassWithColorTheme("text", colorTheme))}>
+          {unavailableNoticeText}
+        </div>
+      </div>
+    </FlexBox>
+  );
+};
