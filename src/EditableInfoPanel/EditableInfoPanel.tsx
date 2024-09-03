7eabd22aed127264b913febd124d171c19c8eb30
soto-sandra <102196999+soto-sandra@users.noreply.github.com>
2024-09-03 15:23:22 -0700

    Add button and tooltip props for the EditableInfoPanel

--- src/EditableInfoPanel/EditableInfoPanel.tsx
+++ src/EditableInfoPanel/EditableInfoPanel.tsx
@@ -1,9 +1,11 @@
 import * as classnames from "classnames";
 import * as PropTypes from "prop-types";
 import * as React from "react";
-import { Button } from "../Button/Button";
+import { Button, Props as ButtonProps } from "../Button/Button";
 import FlexBox from "../flex/FlexBox";
 import FlexItem from "../flex/FlexItem";
+import { Props as TooltipProps } from "../Tooltip/Tooltip";
+import Tooltip from "../Tooltip/Tooltip";
 
 import "./EditableInfoPanel.less";
 
@@ -12,6 +14,12 @@ export interface Props {
   className?: string;
   title?: React.ReactNode;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
+  buttonProps?: ButtonProps;
+  tooltipProps?: EditableInfoPanelTooltipProps;
+}
+
+export interface EditableInfoPanelTooltipProps extends Omit<TooltipProps, "children"> {
+  children?: React.ReactNode | null;
 }
 
 const propTypes = {
@@ -19,26 +27,45 @@ const propTypes = {
   className: PropTypes.string,
   title: PropTypes.node,
   onClick: PropTypes.func.isRequired,
+  buttonProps: PropTypes.object,
+  tooltipProps: PropTypes.object,
 };
 
 const cssClass = {
   CONTAINER: "EditableInfoPanel",
   HEADER: "EditableInfoPanel--header",
   CONTENT: "EditableInfoPanel--content",
+  BUTTON: "EditableInfoPanel--button",
 };
 
 export default class EditableInfoPanel extends React.PureComponent<Props> {
   static propTypes = propTypes;
 
   render() {
-    const { title, children, className, onClick } = this.props;
+    const { title, className, children, onClick, buttonProps, tooltipProps } = this.props;
+
+    const btn = (
+      <Button
+        {...{
+          value: "Edit",
+          size: "small",
+          onClick,
+          ...buttonProps,
+        }}
+      />
+    );
     return (
       <div className={classnames(cssClass.CONTAINER, className)}>
         <FlexBox className={cssClass.HEADER}>
           {title}
           <FlexItem grow />
           <FlexItem>
-            <Button value="Edit" size="small" onClick={onClick} />
+            {tooltipProps && (
+              <Tooltip {...tooltipProps}>
+                <span>{btn}</span>
+              </Tooltip>
+            )}
+            {!tooltipProps && btn}
           </FlexItem>
         </FlexBox>
         <div className={cssClass.CONTENT}>{children}</div>
