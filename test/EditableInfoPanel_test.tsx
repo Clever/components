70168eb8c70e1b436e297a9d9367c6e4eedcd857
soto-sandra <102196999+soto-sandra@users.noreply.github.com>
2024-09-03 17:51:45 -0700

    Make format

--- test/EditableInfoPanel_test.tsx
+++ test/EditableInfoPanel_test.tsx
@@ -5,41 +5,41 @@ import { shallow } from "enzyme";
 import EditableInfoPanel from "../src/EditableInfoPanel";
 
 describe("EditableInfoPanel", () => {
-    it("uses default buttonProps and does not render tooltip when buttonProps and tooltipProps are not provided", () => {
-        const wrapper = shallow(
-            <EditableInfoPanel title="Title" onClick={() => null}>
-                Content
-            </EditableInfoPanel>,
-        );
-        assert.strictEqual(wrapper.find("Button").props().size, "small");
-        assert.strictEqual(wrapper.find("Button").props().value, "Edit");
-        assert.strictEqual(wrapper.find("Tooltip").length, 0);
-    });
-    
-    it("uses buttonProps when provided", () => {
-        const wrapper = shallow(
-            <EditableInfoPanel
-                title="Title"
-                onClick={() => null}
-                buttonProps={{size: "large", value: "New value!"}}
-            >
-                Content
-            </EditableInfoPanel>,
-        );
-        assert.strictEqual(wrapper.find("Button").props().size, "large");
-        assert.strictEqual(wrapper.find("Button").props().value, "New value!");
-    });
+  it("uses default buttonProps and does not render tooltip when buttonProps and tooltipProps are not provided", () => {
+    const wrapper = shallow(
+      <EditableInfoPanel title="Title" onClick={() => null}>
+        Content
+      </EditableInfoPanel>,
+    );
+    assert.strictEqual(wrapper.find("Button").props().size, "small");
+    assert.strictEqual(wrapper.find("Button").props().value, "Edit");
+    assert.strictEqual(wrapper.find("Tooltip").length, 0);
+  });
 
-    it("renders a tooltip when tooltipProps are provided", () => {
-        const wrapper = shallow(
-            <EditableInfoPanel
-                title="Title"
-                onClick={() => null}
-                tooltipProps={{ content: "Tooltip content"}}
-            >
-                Content
-            </EditableInfoPanel>,
-        );
-        assert.strictEqual(wrapper.find("Tooltip").length, 1);
-    });
+  it("uses buttonProps when provided", () => {
+    const wrapper = shallow(
+      <EditableInfoPanel
+        title="Title"
+        onClick={() => null}
+        buttonProps={{ size: "large", value: "New value!" }}
+      >
+        Content
+      </EditableInfoPanel>,
+    );
+    assert.strictEqual(wrapper.find("Button").props().size, "large");
+    assert.strictEqual(wrapper.find("Button").props().value, "New value!");
+  });
+
+  it("renders a tooltip when tooltipProps are provided", () => {
+    const wrapper = shallow(
+      <EditableInfoPanel
+        title="Title"
+        onClick={() => null}
+        tooltipProps={{ content: "Tooltip content" }}
+      >
+        Content
+      </EditableInfoPanel>,
+    );
+    assert.strictEqual(wrapper.find("Tooltip").length, 1);
+  });
 });
